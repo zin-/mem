3ccd2fea3fede352a3c5946b445af4a32370913d
zin- <noyu718@gmail.com>
2022-07-21 09:01:32 +0900

    modify database test
    mem-7 文字列を保存する

--- integration_test/database/database_test.dart
+++ integration_test/database/database_test.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import 'package:mem/database/database.dart';
+import 'package:mem/database/indexed_database.dart';
+import 'package:mem/database/sqlite_database.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -11,30 +14,18 @@ void main() {
     () async {
       const dbName = 'test.db';
       const dbVersion = 1;
-      const tableName = 'tests';
-
-      const integerPkName = 'id';
-      const textFieldName = 'text';
-      final table = DefT(
-        tableName,
-        [
-          DefPK(integerPkName, TypeF.integer, autoincrement: true),
-          DefF(textFieldName, TypeF.text),
-        ],
-      );
 
       final database = await DatabaseFactory.open(
         dbName,
         dbVersion,
-        [
-          table,
-        ],
+        [],
       );
 
-      expect(database.name, dbName);
-      expect(database.version, dbVersion);
-      expect(database.tables.length, 1);
-      expect(database.tables[0].toString(), contains(tableName));
+      if (kIsWeb) {
+        expect(database, const TypeMatcher<IndexedDatabase>());
+      } else {
+        expect(database, const TypeMatcher<SqliteDatabase>());
+      }
     },
   );
 }
