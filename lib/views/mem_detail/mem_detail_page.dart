d7c2e126048504577225dbc25e4bc79f55b8c48b
zin- <noyu718@gmail.com>
2022-07-31 13:25:44 +0900

    patch AsyncValueView to MemDetailPage
    mem-16 Mem一覧

--- lib/views/mem_detail/mem_detail_page.dart
+++ lib/views/mem_detail/mem_detail_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:mem/logger.dart';
+import 'package:mem/views/async_value_view.dart';
 import 'package:mem/views/constants.dart';
 import 'package:mem/views/mem_detail/mem_detail_states.dart';
 
@@ -18,66 +19,58 @@ class MemDetailPage extends StatelessWidget {
         () => Consumer(
           builder: (context, ref, child) => v(
             {},
-            () {
-              final memMapAsyncValue = ref.watch(fetchMemById(_memId));
-
-              return Scaffold(
-                appBar: AppBar(
-                  title: const Text('Detail'),
-                ),
-                body: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Form(
+            () => Scaffold(
+              appBar: AppBar(
+                title: const Text('Detail'),
+              ),
+              body: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Form(
                     key: _formKey,
-                    child: memMapAsyncValue.when(
-                      data: (memDataMap) => Column(
-                        children: [
-                          TextFormField(
-                            initialValue: memDataMap['name'] ?? '',
-                            validator: (value) => (value?.isEmpty ?? false)
-                                ? 'Name is required.'
-                                : null,
-                            onChanged: (value) => ref
-                                .read(memMapProvider(_memId).notifier)
-                                .updatedBy(memDataMap..['name'] = value),
-                          ),
-                        ],
-                      ),
-                      error: (e, s) => const Text('error'),
-                      loading: () => const Text('loading'),
-                    ),
-                  ),
-                ),
-                floatingActionButton: Consumer(
-                  builder: (context, ref, child) {
-                    return FloatingActionButton(
-                      child: const Icon(Icons.save_alt),
-                      onPressed: () {
-                        final memMap = ref.watch(memMapProvider(_memId));
-
-                        if (_formKey.currentState?.validate() ?? false) {
-                          ref.read(saveMem(memMap)).then((saveSuccess) {
-                            if (saveSuccess) {
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(SnackBar(
-                                content:
-                                    Text('Save success. ${memMap['name']}'),
-                                duration: const Duration(
-                                  seconds: defaultDismissDurationSeconds,
+                    child: AsyncValueView(
+                        ref.watch(fetchMemById(_memId)),
+                        (Map<String, dynamic> memDataMap) => Column(
+                              children: [
+                                TextFormField(
+                                  initialValue: memDataMap['name'] ?? '',
+                                  validator: (value) =>
+                                      (value?.isEmpty ?? false)
+                                          ? 'Name is required.'
+                                          : null,
+                                  onChanged: (value) => ref
+                                      .read(memMapProvider(_memId).notifier)
+                                      .updatedBy(memDataMap..['name'] = value),
                                 ),
-                                dismissDirection: DismissDirection.horizontal,
-                              ));
-                            }
-                          });
-                        }
-                      },
-                    );
-                  },
-                ),
-                floatingActionButtonLocation:
-                    FloatingActionButtonLocation.centerFloat,
-              );
-            },
+                              ],
+                            ))),
+              ),
+              floatingActionButton: Consumer(
+                builder: (context, ref, child) {
+                  return FloatingActionButton(
+                    child: const Icon(Icons.save_alt),
+                    onPressed: () {
+                      final memMap = ref.watch(memMapProvider(_memId));
+
+                      if (_formKey.currentState?.validate() ?? false) {
+                        ref.read(saveMem(memMap)).then((saveSuccess) {
+                          if (saveSuccess) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Save success. ${memMap['name']}'),
+                              duration: const Duration(
+                                seconds: defaultDismissDurationSeconds,
+                              ),
+                              dismissDirection: DismissDirection.horizontal,
+                            ));
+                          }
+                        });
+                      }
+                    },
+                  );
+                },
+              ),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerFloat,
+            ),
           ),
         ),
       );
