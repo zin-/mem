7d063d425ae85d6bf76dc7afd78d531c07c769a8
zin- <noyu718@gmail.com>
2022-09-05 23:40:30 +0900

    upgrade flutter version 3.0.3
    wip lint
    wip mem-12 ブランチがプッシュされたときにテストを実行する

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-BRANCH=$1
-git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH "$GITHUB_WORKSPACE/_flutter"
+git clone https://github.com/flutter/flutter.git --depth 1 -b 3.0.3 "$GITHUB_WORKSPACE/_flutter"
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
