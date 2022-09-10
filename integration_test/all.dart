4343c2f2a8fd63fdb133242e748fe618ca2d0367
zin- <noyu718@gmail.com>
2022-09-10 21:04:20 +0900

    add sqlite_database_test all
    wip mem-95 Memo on MemDetail

--- integration_test/all.dart
+++ integration_test/all.dart
@@ -3,9 +3,10 @@ import 'package:flutter/foundation.dart';
 import 'package:mem/database/database_factory.dart';
 import 'package:mem/logger.dart';
 
-import 'database/database_test.dart' as database_test;
 import 'database/database_factory_test.dart' as database_factory_test;
 import 'database/database_on_web_test.dart' as database_on_web_test;
+import 'database/database_test.dart' as database_test;
+import 'database/sqlite_database_test.dart' as sqlite_database_test;
 
 import 'repositories/repository_test.dart' as repository_test;
 import 'repositories/mem_repository_test.dart' as mem_repository_test;
@@ -17,6 +18,7 @@ void main() async {
   DatabaseManager(onTest: true);
 
   database_test.main();
+  sqlite_database_test.main();
   database_factory_test.main();
   if (kIsWeb) {
     database_on_web_test.main();
