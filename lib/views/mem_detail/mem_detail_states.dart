8788a509b9668a503358f3674e68890cca3c9581
zin- <noyu718@gmail.com>
2022-09-11 11:44:36 +0900

    refactoring drop mem.dart
    wip mem-95 Memo on MemDetail

--- lib/views/mem_detail/mem_detail_states.dart
+++ lib/views/mem_detail/mem_detail_states.dart
@@ -1,9 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:mem/logger.dart';
-import 'package:mem/mem.dart';
-import 'package:mem/views/atoms/state_notifier.dart';
 import 'package:mem/repositories/mem_repository.dart';
+import 'package:mem/views/atoms/state_notifier.dart';
 
 const _memIdKey = '_memId';
 
@@ -29,19 +27,17 @@ final fetchMemById =
   (ref, memId) => v(
     {'memId': memId},
     () async {
-      var memMap = ref.read(memMapProvider(memId));
-
-      if (memId != null && !Mem.isSavedMap(memMap)) {
+      if (memId != null && ref.read(memProvider(memId)) == null) {
         try {
           final mem = await MemRepository().shipById(memId);
           ref.read(memProvider(memId).notifier).updatedBy(mem);
-          memMap = mem.toMap();
+          return mem.toMap();
         } catch (e) {
           warn(e);
         }
       }
 
-      return memMap;
+      return ref.read(memMapProvider(memId));
     },
   ),
 );
@@ -84,8 +80,7 @@ final archiveMem = Provider.family<Future<MemEntity>, Map<String, dynamic>>(
   (ref, memMap) => v(
     {'memMap': memMap},
     () async {
-      final archived =
-          await MemRepository().archive(MemEntity.fromMap(memMap));
+      final archived = await MemRepository().archive(MemEntity.fromMap(memMap));
       ref.read(memProvider(archived.id).notifier).updatedBy(archived);
       return archived;
     },
