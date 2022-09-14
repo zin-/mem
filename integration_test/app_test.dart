618b849cdd1d113382e8e3ff9d5ed5785b6df4f0
zin- <noyu718@gmail.com>
2022-09-14 17:14:26 +0900

    refactoring
    wip mem-95 Memo on MemDetail

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:mem/logger.dart';
 import 'package:mem/database/database_factory.dart';
 import 'package:mem/repositories/mem_item_repository.dart';
