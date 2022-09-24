4d410c70a71ec0317e53b7411e85c62159ac7227
zin- <noyu718@gmail.com>
2022-09-24 11:15:30 +0900

    drop settings_ui
    wip mem-24 Done/Undone

--- lib/views/mems/mem_list/mem_list_filter.dart
+++ lib/views/mems/mem_list/mem_list_filter.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mem/l10n.dart';
 import 'package:mem/logger.dart';
 import 'package:mem/views/mems/mem_list/mem_list_page_states.dart';
-import 'package:settings_ui/settings_ui.dart';
 
 const height = 250.0;
 
@@ -13,52 +12,55 @@ class MemListFilter extends StatelessWidget {
   @override
   Widget build(BuildContext context) => t(
         {},
-        () => Consumer(
-          builder: (context, ref, child) => SettingsList(
-            sections: [
-              SettingsSection(
-                title: Text(L10n().archiveFilterTitle()),
-                tiles: [
-                  SettingsTile.switchTile(
-                    leading: const Icon(Icons.unarchive),
-                    title: Text(L10n().showNotArchivedLabel()),
-                    initialValue: ref.watch(showNotArchivedProvider),
-                    onToggle: (value) => ref
-                        .watch(showNotArchivedProvider.notifier)
-                        .updatedBy(value),
-                  ),
-                  SettingsTile.switchTile(
-                    leading: const Icon(Icons.archive),
-                    title: Text(L10n().showArchivedLabel()),
-                    initialValue: ref.watch(showArchivedProvider),
-                    onToggle: (value) => ref
-                        .watch(showArchivedProvider.notifier)
-                        .updatedBy(value),
-                  ),
-                ],
-              ),
-              // SettingsSection(
-              //   title: Text(L10n().doneFilterTitle()),
-              //   tiles: [
-              //     SettingsTile.switchTile(
-              //       leading: const Icon(Icons.check_box_outline_blank),
-              //       title: Text(L10n().showNotDoneLabel()),
-              //       initialValue: ref.watch(showNotDoneProvider),
-              //       onToggle: (value) => ref
-              //           .watch(showNotDoneProvider.notifier)
-              //           .updatedBy(value),
-              //     ),
-              //     SettingsTile.switchTile(
-              //       leading: const Icon(Icons.check_box_outlined),
-              //       title: Text(L10n().showDoneLabel()),
-              //       initialValue: ref.watch(showDoneProvider),
-              //       onToggle: (value) =>
-              //           ref.watch(showDoneProvider.notifier).updatedBy(value),
-              //     ),
-              //   ],
-              // ),
-            ],
-          ),
-        ),
+        () {
+          final sections = [
+            Consumer(
+              builder: (context, ref, child) {
+                return Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(L10n().archiveFilterTitle()),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.unarchive),
+                        Text(L10n().showNotArchivedLabel()),
+                        Switch(
+                          value: ref.watch(showNotArchivedProvider),
+                          onChanged: (value) => ref
+                              .read(showNotArchivedProvider.notifier)
+                              .updatedBy(value),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Icon(Icons.archive),
+                        Text(L10n().showArchivedLabel()),
+                        Switch(
+                          value: ref.watch(showArchivedProvider),
+                          onChanged: (value) => ref
+                              .read(showArchivedProvider.notifier)
+                              .updatedBy(value),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+          ];
+          return ListView.builder(
+            itemCount: sections.length,
+            itemBuilder: (context, index) {
+              return sections[index];
+            },
+          );
+        },
       );
 }
