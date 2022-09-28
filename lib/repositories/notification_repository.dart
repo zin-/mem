00788f877351e301fbd51728757193f9e9eb31db
zin- <noyu718@gmail.com>
2022-09-28 13:18:37 +0900

    add flutter_local_notifications
    wip mem-29 On time notification

--- lib/repositories/notification_repository.dart
+++ lib/repositories/notification_repository.dart
@@ -1,1 +1,26 @@
-class NotificationRepository {}
+class NotificationRepository {
+  static NotificationRepository? _instance;
+
+  NotificationRepository._();
+
+  factory NotificationRepository() {
+    var tmp = _instance;
+    if (tmp == null) {
+      throw Exception('Call initialize'); // coverage:ignore-line
+    } else {
+      return tmp;
+    }
+  }
+
+  factory NotificationRepository.initialize() {
+    var tmp = NotificationRepository._();
+
+    _instance = tmp;
+    return tmp;
+  }
+
+  factory NotificationRepository.withMock(NotificationRepository mock) {
+    _instance = mock;
+    return mock;
+  }
+}
