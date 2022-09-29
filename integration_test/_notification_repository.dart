8e35f2fd5641ae1318b627d44a47e9edf7953345
zin- <noyu718@gmail.com>
2022-09-30 08:27:52 +0900

    add label
    wip mem-29 On time notification

--- integration_test/_notification_repository.dart
+++ integration_test/_notification_repository.dart
@@ -4,59 +4,61 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mem/repositories/notification_repository.dart';
 
 void testNotificationRepository() {
-  if (Platform.isAndroid) {
-    group(
-      'Create instance',
-      () {
-        test(
-          ': no initialized',
-          () {
-            expect(
-              () => NotificationRepository(),
-              throwsA((e) => e is Exception),
-            );
-          },
-          tags: 'Medium',
-        );
+  group('NotificationRepository test', () {
+    if (Platform.isAndroid) {
+      group(
+        'Create instance',
+        () {
+          test(
+            ': no initialized',
+            () {
+              expect(
+                () => NotificationRepository(),
+                throwsA((e) => e is Exception),
+              );
+            },
+            tags: 'Medium',
+          );
 
-        test(
-          ': initialized',
-          () {
-            final initialized = NotificationRepository.initialize();
+          test(
+            ': initialized',
+            () {
+              final initialized = NotificationRepository.initialize();
 
-            final notificationRepository = NotificationRepository();
+              final notificationRepository = NotificationRepository();
 
-            expect(notificationRepository, initialized);
-          },
-          tags: 'Medium',
-        );
-      },
-    );
+              expect(notificationRepository, initialized);
+            },
+            tags: 'Medium',
+          );
+        },
+      );
 
-    group(
-      'Operating',
-      () {
-        late NotificationRepository notificationRepository;
+      group(
+        'Operating',
+        () {
+          late NotificationRepository notificationRepository;
 
-        setUpAll(() {
-          notificationRepository = NotificationRepository.initialize();
-        });
+          setUpAll(() {
+            notificationRepository = NotificationRepository.initialize();
+          });
 
-        testWidgets(
-          ': receive',
-          (widgetTester) async {
-            await notificationRepository.receive(
-              1,
-              'title',
-              DateTime.now().add(const Duration(days: 1)),
-            );
+          testWidgets(
+            ': receive',
+            (widgetTester) async {
+              await notificationRepository.receive(
+                1,
+                'title',
+                DateTime.now().add(const Duration(days: 1)),
+              );
 
-            // dev(result);
-            // TODO 通知されていることをcheckする
-          },
-          tags: 'Medium',
-        );
-      },
-    );
-  }
+              // dev(result);
+              // TODO 通知されていることをcheckする
+            },
+            tags: 'Medium',
+          );
+        },
+      );
+    }
+  });
 }
