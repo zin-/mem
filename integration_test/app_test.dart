906e1191404af772cc3c5481e827fe7da2aa782d
zin- <noyu718@gmail.com>
2022-09-30 09:23:49 +0900

    refactoring extract _database_tuple_repository.dart
    wip mem-29 On time notification

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,6 +2,7 @@ import 'package:integration_test/integration_test.dart';
 import 'package:mem/logger.dart';
 import 'package:mem/database/database_factory.dart';
 
+import '_database_tuple_repository.dart';
 import '_edge_scenario.dart';
 import '_memo_scenario.dart';
 import '_notification_repository.dart';
@@ -15,6 +16,7 @@ void main() {
   Logger(level: Level.verbose);
   DatabaseManager(onTest: true);
 
+  testDatabaseTupleRepository();
   testNotificationRepository();
 
   testMemoScenario();
