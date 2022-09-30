838c6b2e5e0e514efbba8c46fd6850ffd1b896de
zin- <noyu718@gmail.com>
2022-09-30 09:24:27 +0900

    refactoring move directory
    wip mem-29 On time notification

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -2,10 +2,10 @@ import 'package:integration_test/integration_test.dart';
 import 'package:mem/logger.dart';
 import 'package:mem/database/database_factory.dart';
 
-import '_database_tuple_repository.dart';
+import 'repositories/_database_tuple_repository.dart';
 import '_edge_scenario.dart';
 import '_memo_scenario.dart';
-import '_notification_repository.dart';
+import 'repositories/_notification_repository.dart';
 import '_todo_scenario.dart';
 
 const defaultDuration = Duration(seconds: 1);
