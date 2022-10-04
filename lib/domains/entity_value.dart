6c580f00e0442b893f1718250c66c78cc985d83a
zin- <noyu718@gmail.com>
2022-10-04 09:23:32 +0900

    coverage
    wip mem-29 On time notification

--- lib/domains/entity_value.dart
+++ lib/domains/entity_value.dart
@@ -14,12 +14,4 @@ abstract class EntityValue {
   bool isSaved() => id != null && createdAt != null;
 
   bool isArchived() => isSaved() && archivedAt != null;
-
-  @override
-  String toString() => {
-        'id': id,
-        'createdAt': createdAt,
-        'updatedAt': updatedAt,
-        'archivedAt': archivedAt,
-      }.toString();
 }
