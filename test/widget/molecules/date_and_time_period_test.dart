30368aa37bf08eabe62077165992efb388bd78a0
zin- <noyu718@gmail.com>
2022-10-18 09:37:56 +0900

    ignore
    wip mem-62 Date time Period picker
    
    pending mem-41 Period

--- test/widget/molecules/date_and_time_period_test.dart
+++ test/widget/molecules/date_and_time_period_test.dart
@@ -1,19 +1,19 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mem/views/molecules/date_and_time_period.dart';
-
-import '../../_helpers.dart';
-
-void main() {
-  group('View', () {
-    testWidgets(
-      'description',
-      (widgetTester) async {
-        await runWidget(
-          widgetTester,
-          DateAndTimePeriodFields(),
-        );
-      },
-      skip: true,
-    );
-  });
-}
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:mem/views/molecules/date_and_time_period.dart';
+//
+// import '../../_helpers.dart';
+//
+// void main() {
+//   group('View', () {
+//     testWidgets(
+//       'description',
+//       (widgetTester) async {
+//         await runWidget(
+//           widgetTester,
+//           DateAndTimePeriodFields(),
+//         );
+//       },
+//       skip: true,
+//     );
+//   });
+// }
