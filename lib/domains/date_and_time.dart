bb65869220d9693cd3ae225fb7fb28c4b4fc5fc5
zin- <noyu718@gmail.com>
2022-11-04 09:47:52 +0900

    add domain/date_and_time

---
+++ lib/domains/date_and_time.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class DateAndTime extends DateTime {
+  bool isAllDay = false;
+
+  DateAndTime(int year,
+      [int month = 1,
+      int day = 1,
+      int? hour,
+      int? minute,
+      int? second,
+      int? millisecond,
+      int? microsecond])
+      : isAllDay = hour == null &&
+            minute == null &&
+            second == null &&
+            millisecond == null &&
+            microsecond == null,
+        super(
+          year,
+          month,
+          day,
+          hour ?? 0,
+          minute ?? 0,
+          second ?? 0,
+          millisecond ?? 0,
+          microsecond ?? 0,
+        );
+
+  DateAndTime.from(
+    DateTime dateTime, {
+    TimeOfDay? timeOfDay,
+  }) : this(
+          dateTime.year,
+          dateTime.month,
+          dateTime.day,
+          timeOfDay?.hour,
+          timeOfDay?.minute,
+          null,
+          null,
+          null,
+        );
+
+  DateAndTime.now({bool allDay = false})
+      : this.from(
+          DateTime.now(),
+          timeOfDay: allDay ? null : TimeOfDay.now(),
+        );
+
+  DateTime get dateTime => DateTime(year, month, day);
+
+  TimeOfDay? get timeOfDay => isAllDay ? null : TimeOfDay.fromDateTime(this);
+
+  @override
+  String toString() => '${super.toString()}, isAllDay: $isAllDay';
+}
+
+class DateAndTimePeriod {
+  final DateAndTime? start;
+  final DateAndTime? end;
+
+  DateAndTimePeriod(this.start, this.end) {
+    if (start != null && end != null) {
+      assert(start!.isBefore(end!));
+    }
+  }
+
+  @override
+  String toString() {
+    return {
+      'start': start,
+      'end': end,
+    }.toString();
+  }
+}
---
+++ test/domain/date_and_time_test.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mem/domains/date_and_time.dart';
+
+import '../_helpers.dart';
+
+void main() {
+  group('Create instance', () {
+    group(': now', () {
+      test(
+        ': allDay is false',
+            () {
+          final dateAndTime = DateAndTime.now();
+          final dateTime = DateTime.fromMicrosecondsSinceEpoch(
+              dateAndTime.microsecondsSinceEpoch);
+
+          expect(dateAndTime, isA<DateAndTime>());
+          expect(dateAndTime, isA<DateTime>());
+
+          final string = dateAndTime.toString();
+          expect(string, '${dateTime.toString()}, isAllDay: false');
+        },
+        tags: TestSize.small,
+      );
+
+      test(
+        ': allDay is true',
+            () {
+          final dateAndTime = DateAndTime.now(allDay: true);
+          final dateTime = DateTime.fromMicrosecondsSinceEpoch(
+              dateAndTime.microsecondsSinceEpoch);
+
+          expect(dateAndTime, isA<DateAndTime>());
+          expect(dateAndTime, isA<DateTime>());
+
+          final string = dateAndTime.toString();
+          expect(string, '${dateTime.toString()}, isAllDay: true');
+        },
+        tags: TestSize.small,
+      );
+    });
+  });
+
+  group('DateAndTimePeriod', () {
+    group('Create Instance', () {
+      test(
+        'Both null',
+            () {
+          final dateAndTimePeriod = DateAndTimePeriod(null, null);
+
+          expect(dateAndTimePeriod.toString(), '{start: null, end: null}');
+        },
+      );
+
+      test(
+        'Start is after end',
+            () {
+          final now = DateTime.now();
+
+          expect(
+                () => DateAndTimePeriod(
+              DateAndTime.from(now.add(const Duration(days: 1))),
+              DateAndTime.from(now),
+            ),
+            throwsA((e) => e is AssertionError),
+          );
+        },
+      );
+    });
+  });
+}
