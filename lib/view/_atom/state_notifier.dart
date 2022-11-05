b214a862929a224f073ffbd0c868ccf97237522f
zin- <noyu718@gmail.com>
2022-11-05 10:59:24 +0900

    change for log

--- lib/view/_atom/state_notifier.dart
+++ lib/view/_atom/state_notifier.dart
@@ -1,11 +1,31 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mem/logger.dart';
+
+const _jsonEncoderIndent = '  ';
 
 class ValueStateNotifier<T> extends StateNotifier<T> {
   ValueStateNotifier(super.state);
 
   // TODO performance 値が変わっていたら通知する形にしたい
   T updatedBy(T value) => state = value;
+
+  @override
+  String toString() {
+    String content;
+    if (state is Map || state is Iterable) {
+      final encoder = JsonEncoder.withIndent(
+        _jsonEncoderIndent,
+        (object) => object.toString(),
+      );
+      content = encoder.convert(state);
+    } else {
+      content = state.toString();
+    }
+
+    return 'ValueStateNotifier: $content';
+  }
 }
 
 class ListValueStateNotifier<T> extends ValueStateNotifier<List<T>?> {
