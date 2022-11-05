c0bece7d8b56d8d6a7f2625af56b72cea5c189b4
zin- <noyu718@gmail.com>
2022-11-05 11:22:22 +0900

    modify failed test

--- integration_test/_helpers.dart
+++ integration_test/_helpers.dart
@@ -5,7 +5,6 @@ import 'package:mem/main.dart' as app;
 import 'package:mem/domain/mem.dart';
 import 'package:mem/services/mem_service.dart';
 import 'package:mem/view/mems/mem_detail/mem_items_view.dart';
-import 'package:mem/listAndDetails//mem_list_item_view.dart';
 import 'package:mem/view/mems/mem_name.dart';
 import 'package:mem/listAndDetails/mem_notify_at.dart';
 import 'package:mem/view/molecules/date_and_time_text_form_field.dart';
@@ -106,10 +105,7 @@ final clearDateAndTimeIconFinder = find.descendant(
   of: find.byType(DateAndTimeTextFormField),
   matching: find.byIcon(Icons.clear),
 );
-final memNotifyAtTextFinder = find.descendant(
-  of: find.byType(MemListItemView),
-  matching: find.byType(MemNotifyAtText),
-);
+final memNotifyAtTextFinder = find.byType(MemNotifyAtText);
 
 Future<void> prepareSavedMem(
   String memName,
