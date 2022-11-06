78419bf69f27b45c53e5129edf6b62eb0bd9c50a
zin- <noyu718@gmail.com>
2022-11-06 14:09:43 +0900

    add DateAndTimePeriod
    wip mem-34 ActList

---
+++ lib/act/domain/date_and_time_period.dart
@@ -0,0 +1,11 @@
+import 'package:mem/domain/date_and_time.dart';
+
+class DateAndTimePeriod {
+  final DateAndTime? start;
+  final DateAndTime? end;
+
+  DateAndTimePeriod({this.start, this.end}) {
+    assert(start != null || end != null);
+    if (start != null && end != null) assert(start!.compareTo(end!) <= 0);
+  }
+}
---
+++ test/act/date_and_time_period_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mem/act/domain/date_and_time_period.dart';
+import 'package:mem/domain/date_and_time.dart';
+
+void main() {
+  group('Create instance', () {
+    test(': with start', () {
+      final start = DateAndTime.now();
+      final dateAndTimePeriod = DateAndTimePeriod(
+        start: start,
+      );
+
+      expect(dateAndTimePeriod.start, start);
+      expect(dateAndTimePeriod.end, null);
+    });
+    test(': with end', () {
+      final end = DateAndTime.now();
+      final dateAndTimePeriod = DateAndTimePeriod(
+        end: end,
+      );
+
+      expect(dateAndTimePeriod.start, null);
+      expect(dateAndTimePeriod.end, end);
+    });
+    test(': with start and end', () {
+      final now = DateAndTime.now();
+      final dateAndTimePeriod = DateAndTimePeriod(start: now, end: now);
+
+      expect(dateAndTimePeriod.start, now);
+      expect(dateAndTimePeriod.end, now);
+    });
+
+    test(': start and end are null', () {
+      expect(() => DateAndTimePeriod(), throwsAssertionError);
+    });
+    test(': start is after end', () {
+      final start = DateAndTime.now();
+      final end = DateAndTime.from(start.subtract(const Duration(days: 1)));
+
+      expect(
+        () => DateAndTimePeriod(start: start, end: end),
+        throwsAssertionError,
+      );
+    });
+  });
+}
