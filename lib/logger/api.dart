5a65b8a6e32c5e727a30edc55f3e3aa734a08dce
zin- <noyu718@gmail.com>
2022-11-06 15:45:28 +0900

    move for Functional architecture
    wip mem-34 ActList

--- lib/logger/api.dart
+++ lib/logger/api.dart
@@ -1,13 +1,48 @@
-import 'package:mem/logger.dart';
+import 'package:flutter/foundation.dart';
 import 'package:mem/repositories/log_repository.dart';
+import 'package:mem/services/log_service.dart';
+
+const _filePath = 'mem/logger/api.dart';
+
+LogService _logService = LogService(
+  Level.error,
+  ignoreFilePaths: [_filePath],
+);
+
+void initializeLogger([Level? logLevel]) {
+  LogService.reset();
+  _logService = LogService(
+    logLevel ?? (kDebugMode ? Level.trace : Level.error),
+    ignoreFilePaths: [_filePath],
+  );
+}
 
 T v<T>(
   Map<String, dynamic>? arguments,
   T Function() function, {
   @Deprecated('Allow under develop only') bool debug = false,
 }) =>
-    logService.functionLog(
+    _logService.functionLog(
       function,
       arguments: arguments,
       level: debug ? Level.debug : Level.verbose,
     );
+
+T t<T>(
+    Map<String, dynamic>? args,
+    T Function() function, {
+      @Deprecated('Allow under develop only') bool debug = false,
+    }) =>
+    _logService.functionLog(
+      function,
+      arguments: args,
+      level: debug ? Level.debug : Level.trace,
+    );
+
+// coverage:ignore-start
+@Deprecated('Allow under develop only')
+T? dev<T>([T? message]) {
+  _logService.log(message ?? 'Under development.', level: Level.debug);
+  return message;
+}
+// coverage:ignore-end
