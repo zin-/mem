252abd4900068dbfad2a14c23994105bf420d23c
zin- <noyu718@gmail.com>
2022-11-24 08:48:33 +0900

    define RepositoryV2
    mem-36 Abstraction List & Details

---
+++ lib/repositories/_repository_v2.dart
@@ -0,0 +1,16 @@
+import '_entity_v2.dart';
+import 'i/type.dart';
+
+abstract class RepositoryV2<E extends EntityV2, Payload> {
+  Future<Payload> receive(Payload payload);
+
+  Future<List<Payload>> ship(Conditions conditions);
+
+  Future<Payload> replace(Payload payload);
+
+  Future<Payload> archive(Payload payload);
+
+  Future<Payload> unarchive(Payload payload);
+
+  Future<List<Payload>> waste(Conditions conditions);
+}
---
+++ lib/repositories/i/type.dart
@@ -0,0 +1,5 @@
+typedef AttributeName = String;
+
+abstract class Condition {}
+
+typedef Conditions = Map<AttributeName, Condition>;
