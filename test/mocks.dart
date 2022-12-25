84e9677b02161a3e49c9d8f4f60c73f4d726a239
zin- <noyu718@gmail.com>
2022-12-25 12:34:27 +0900

    add FIXME
    pend mem-35 Abstraction Repository

--- test/mocks.dart
+++ test/mocks.dart
@@ -5,6 +5,8 @@ import 'package:mem/repositories/mem_repository.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateMocks([
+  // FIXME RepositoryではなくTableをmockする
+  //  Repositoryはシステム固有の処理であるのに対して、Tableは永続仮想をラップする役割を持つため
   MemRepository,
   MemRepositoryV2,
   MemItemRepository,
