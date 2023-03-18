d91036bb153acac56eb99feebe923bae6a105bcc
zin- <noyu718@gmail.com>
2023-03-18 13:27:57 +0900

    refactor

--- lib/act_counter/all.dart
+++ lib/act_counter/all.dart
@@ -10,21 +10,22 @@ const appId = 'zin.playground.mem';
 const actCounter = 'act_counters';
 const memIdParamName = 'mem_id';
 
-void backgroundCallback(Uri? uri) async {
-  trace(uri);
+void backgroundCallback(Uri? uri) => t(
+      {'uri': uri},
+      () async {
+        if (uri != null && uri.scheme == uriSchema && uri.host == appId) {
+          await openDatabase();
 
-  if (uri != null && uri.scheme == uriSchema && uri.host == appId) {
-    await openDatabase();
+          if (uri.pathSegments.contains(actCounter)) {
+            final memId = uri.queryParameters[memIdParamName];
 
-    if (uri.pathSegments.contains(actCounter)) {
-      final memId = uri.queryParameters[memIdParamName];
-
-      if (memId != null) {
-        await ActCounterService().increment(int.parse(memId));
-      }
-    }
-  }
-}
+            if (memId != null) {
+              await ActCounterService().increment(int.parse(memId));
+            }
+          }
+        }
+      },
+    );
 
 void initializeActCounter() {
   if (!Platform.isWindows) {
