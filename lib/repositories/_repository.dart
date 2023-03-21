e3656994b9d307ef67b225444711dedb08000807
zin- <noyu718@gmail.com>
2023-03-21 11:22:37 +0900

    add TODO

--- lib/repositories/_repository.dart
+++ lib/repositories/_repository.dart
@@ -1,7 +1,7 @@
-@Deprecated('use instead RepositoryV2')
+// TODO @Deprecated('use instead RepositoryV2')
 abstract class Repository<E extends Entity, Result> {
   Result receive(E entity);
 }
 
-@Deprecated('use instead EntityV2')
+// TODO @Deprecated('use instead EntityV2')
 abstract class Entity {}
