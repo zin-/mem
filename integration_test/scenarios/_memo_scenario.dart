29166a6724d981f9419583c215e3da82dc3d6eb3
zin- <noyu718@gmail.com>
2023-04-14 10:33:46 +0900

    add FIXME
    wip mem-133 `Integration test(android)` が成功するようにする

--- integration_test/scenarios/_memo_scenario.dart
+++ integration_test/scenarios/_memo_scenario.dart
@@ -105,6 +105,10 @@ void testMemoScenario() => group(
             const enteringMemName = 'entering mem name';
             const enteringMemMemo = 'entering mem memo';
 
+            // FIXME 文字入力が動作しない場合がある
+            //  改善しようとしたが、`pump`や`pumpAndSettle`を前後に追加するだけでは改善しなかった
+            //    https://github.com/zin-/mem/issues/133
+            // プロダクションコードの方に問題がある可能性があるので、後続のリファクタリングと同時に解決を目指す
             await widgetTester.enterText(
                 memNameTextFormFieldFinder, enteringMemName);
             await widgetTester.enterText(
