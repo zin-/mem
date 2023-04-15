7df8803d9d1434b8fcffb617e322df7545487455
zin <noyu718@gmail.com>
2023-04-15 11:28:39 +0900

    fixed bug (#140)
    くだらないバグすぎてテストの追加は不要と判断した
    具体的には、ログ出力のためのMapのkeyにString以外の値を設定しようとしてエラーになっていた
    
    mem-137 アプリ再起動後MemItemの表示でエラーが発生している

--- lib/act_counter/home_widget_accessor.dart
+++ lib/act_counter/home_widget_accessor.dart
@@ -4,23 +4,25 @@ import 'package:mem/logger/i/api.dart';
 
 class HomeWidgetAccessor {
   Future<int> initialize(
-          String methodChannelName, String initializeMethodName) =>
+    String methodChannelName,
+    String initializeMethodName,
+  ) =>
       v(
         {
-          methodChannelName: methodChannelName,
-          initializeMethodName: initializeMethodName,
+          'methodChannelName': methodChannelName,
+          'initializeMethodName': initializeMethodName,
         },
         () async => (await MethodChannel(methodChannelName)
             .invokeMethod<int>(initializeMethodName))!,
       );
 
   Future<bool> saveWidgetData(String key, dynamic value) => v(
-        {key: key, value: value},
+        {'key': key, 'value': value},
         () async => (await HomeWidget.saveWidgetData(key, value))!,
       );
 
   Future<bool> updateWidget(String widgetName) => v(
-        {widgetName: widgetName},
+        {'widgetName': widgetName},
         () async => (await HomeWidget.updateWidget(name: widgetName))!,
       );
 
