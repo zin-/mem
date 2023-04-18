5963a5c28382bbd2a32ec70922381c95e6307bc0
zin- <noyu718@gmail.com>
2023-04-18 13:59:26 +0900

    wip add `LogRepositoryV2`
    mem-139 ログレベル`debug`に設定してもログが表示されない

---
+++ lib/logger/log_repository_v2.dart
@@ -0,0 +1,31 @@
+import 'package:mem/frame_work/repository_v3.dart';
+import 'package:mem/logger/log_entity.dart';
+import 'package:mem/logger/logger_wrapper_v2.dart';
+
+class LogRepositoryV2 extends RepositoryV3<Log, void> {
+  LoggerWrapperV2 _loggerWrapper;
+
+  @override
+  void receive(Log payload) => _loggerWrapper.log(
+        payload.level,
+        payload.message,
+        payload.error,
+        payload.stackTrace,
+      );
+
+  LogRepositoryV2._(this._loggerWrapper);
+
+  static LogRepositoryV2? _instance;
+
+  factory LogRepositoryV2(LoggerWrapperV2 loggerWrapperV2) {
+    var tmp = _instance;
+
+    if (tmp == null) {
+      _instance = tmp = LogRepositoryV2._(
+        loggerWrapperV2,
+      );
+    }
+
+    return tmp;
+  }
+}
