3b1602e44c1df677426d29f14115cad36b791bf7
zin- <noyu718@gmail.com>
2023-04-22 09:24:36 +0900

    move doc `home_widget`
    mem-146 ロードマップを考える

--- lib/act_counter/home_widget_accessor.dart
+++ lib/act_counter/home_widget_accessor.dart
@@ -2,6 +2,20 @@ import 'package:flutter/services.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:mem/logger/i/api.dart';
 
+/// # Home widget
+///
+/// Flutterにおける正式名称が"Home widget"なのかもはっきりしていない
+///
+/// ## Package
+///
+/// - [home_widget](https://pub.dev/packages/home_widget)
+///   - これを利用するので、暫定的に"Home widget"としている
+/// - [workmanager](https://pub.dev/packages/workmanager)
+///   - home_widgetで利用しているので、README通りに利用する
+///
+/// ## Reference
+///
+/// - https://developer.android.com/guide/topics/appwidgets?hl=ja
 class HomeWidgetAccessor {
   Future<int> initialize(
     String methodChannelName,
