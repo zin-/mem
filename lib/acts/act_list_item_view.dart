d1b09ea4fc79c4a15beb50b91d1f53b7b3477c0f
zin- <noyu718@gmail.com>
2023-05-18 17:54:50 +0900

    change `ActListItemView`
    wip mem-156 Start and finish act

--- lib/acts/act_list_item_view.dart
+++ lib/acts/act_list_item_view.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mem/logger/i/api.dart';
+import 'package:mem/gui/date_and_time/date_and_time_period_view.dart';
 
 import '../../core/act.dart';
 
-class ActListItemView extends StatelessWidget {
+class ActListItemView extends ListTile {
   final Act act;
 
-  const ActListItemView(this.act, {super.key});
-
-  @override
-  Widget build(BuildContext context) => v(
-        {'act': act},
-        () {
-          return Text(act.toString());
-        },
-      );
+  ActListItemView(this.act, {super.key})
+      : super(
+          title: DateAndTimePeriodTexts(act.period),
+        );
 }
