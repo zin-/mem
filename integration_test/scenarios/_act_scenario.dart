06e845dc4d7ac9939ee7439cf3a2dc3e4eccd7cc
zin- <noyu718@gmail.com>
2023-05-18 18:07:55 +0900

    wip add `ActFab`
    wip mem-156 Start and finish act

--- integration_test/scenarios/_act_scenario.dart
+++ integration_test/scenarios/_act_scenario.dart
@@ -4,6 +4,8 @@ import 'package:integration_test/integration_test.dart';
 import 'package:mem/acts/act_list_item_view.dart';
 import 'package:mem/database/database_manager.dart';
 import 'package:mem/main.dart' as app;
+import 'package:mem/repositories/i/_database_tuple_entity_v2.dart';
+import 'package:mem/repositories/mem_entity.dart';
 
 import '../_helpers.dart';
 
@@ -16,6 +18,37 @@ void main() {
 }
 
 void testActScenario() => group('Act scenario', () {
+      const savedMemName = 'Memo scenario: V2: saved mem name';
+
+      setUp(() async {
+        final memTable =
+            (await DatabaseManager(onTest: true).open(app.databaseDefinition))
+                .getTable(memTableDefinition.name);
+
+        await memTable.insert({
+          defMemName.name: savedMemName,
+          createdAtColumnName: DateTime.now(),
+        });
+      });
+
+      testWidgets(
+        ': start act.',
+        (widgetTester) async {
+          await app.main();
+          await widgetTester.pumpAndSettle();
+
+          await widgetTester.tap(find.text(savedMemName));
+          await widgetTester.pumpAndSettle();
+
+          await widgetTester.tap(showActPageIconFinder);
+          await widgetTester.pumpAndSettle();
+
+          await widgetTester.tap(find.byIcon(Icons.play_arrow));
+
+          expect(1, 1);
+        },
+      );
+
       testWidgets(
         'Save act',
         (widgetTester) async {
--- lib/acts/add_act_fab.dart
+++ lib/acts/add_act_fab.dart
@@ -5,6 +5,30 @@ import 'package:mem/acts/act_list_page_states.dart';
 import 'package:mem/core/mem.dart';
 import 'package:mem/logger/i/api.dart';
 
+class ActFab extends ConsumerWidget {
+  final MemId _memId;
+
+  const ActFab(this._memId, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final actList = ref.watch(actListProvider(_memId));
+
+    // TODO 最新のActがない、もしくは終了している場合はStart
+    //  そうでない場合はFinishを出す
+
+    return _StartActFab();
+  }
+}
+
+class _StartActFab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
 class AddActFab extends ConsumerWidget {
   final MemId _memId;
 
