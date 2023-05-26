fcdd7d7028f335536a813fd8531f6a08526c3f2a
zin- <noyu718@gmail.com>
2023-05-26 09:32:17 +0900

    fixup! move `DateAndTimePeriod`

--- lib/acts/act_list_item_view.dart
+++ lib/acts/act_list_item_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mem/gui/date_and_time/date_and_time_period_view.dart';
+import 'package:mem/component/view/date_and_time/date_and_time_period_view.dart';
 
 import '../../core/act.dart';
 
