8adea6f48159f773cae253258fcc14976cb9093b
zin- <noyu718@gmail.com>
2023-06-13 10:00:56 +0900

    coverage `Task scenario`
    wip #161

--- integration_test/scenarios/task_scenario.dart
+++ integration_test/scenarios/task_scenario.dart
@@ -1,108 +1,133 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:mem/database/database.dart';
+import 'package:mem/database/database_manager.dart';
 import 'package:mem/main.dart' as app;
+import 'package:mem/repositories/_database_tuple_repository.dart';
+import 'package:mem/repositories/mem_entity.dart';
 
 import '../_helpers.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() async {
-    await clearDatabase();
-  });
-
   testTaskScenario();
 }
 
-void testTaskScenario() => group(
-      ': Task scenario',
-      () {
-        testWidgets(
-          ': Set Period.',
-          (widgetTester) async {
-            final now = DateTime.now();
+const scenarioName = 'Task scenario';
 
-            await app.main();
-            await widgetTester.pumpAndSettle();
-            await widgetTester.pumpAndSettle();
-            await widgetTester.pumpAndSettle();
+void testTaskScenario() => group(': $scenarioName', () {
+      late final Database db;
 
-            await widgetTester.tap(newMemFabFinder);
-            await widgetTester.pumpAndSettle();
+      setUpAll(() async {
+        db = await DatabaseManager(onTest: true).open(app.databaseDefinition);
+      });
+      setUp(() async {
+        final memTable = db.getTable(memTableDefinition.name);
 
-            expect(find.text('M/d/y'), findsNWidgets(2));
-            expect(calendarIconFinder, findsNWidgets(2));
-            expect(switchFinder, findsNWidgets(2));
-            expect(timeIconFinder, findsNothing);
-            await widgetTester.tap(calendarIconFinder.at(0));
-            await widgetTester.pumpAndSettle();
+        await memTable.insert({
+          defMemName.name: '$scenarioName: has period',
+          defMemStartOn.name: DateTime.now(),
+          createdAtColumnName: DateTime.now(),
+        });
+        await memTable.insert({
+          defMemName.name: '$scenarioName: no period',
+          defMemStartOn.name: null,
+          defMemStartAt.name: null,
+          defMemEndOn.name: null,
+          defMemEndAt.name: null,
+          createdAtColumnName: DateTime.now(),
+        });
+      });
+      tearDownAll(() async {
+        await DatabaseManager(onTest: true).delete(app.databaseDefinition.name);
+      });
 
-            const pickingStartDate = 1;
-            await widgetTester.tap(find.text('$pickingStartDate'));
-            await widgetTester.tap(find.text('OK'));
-            await widgetTester.pumpAndSettle();
+      testWidgets(
+        ': Set Period.',
+        (widgetTester) async {
+          final now = DateTime.now();
 
-            final startDate = '${now.month}/$pickingStartDate/${now.year}';
-            expect(
-              (widgetTester.widget(find.byType(TextFormField).at(1))
-                      as TextFormField)
-                  .initialValue,
-              startDate,
-            );
-            expect(timeIconFinder, findsNothing);
+          await app.main();
+          await widgetTester.pumpAndSettle();
+          await widgetTester.pumpAndSettle();
+          await widgetTester.pumpAndSettle();
 
-            await widgetTester.tap(switchFinder.at(0));
-            await widgetTester.pumpAndSettle();
+          await widgetTester.tap(newMemFabFinder);
+          await widgetTester.pumpAndSettle();
 
-            await widgetTester.tap(find.text('OK'));
-            await widgetTester.pumpAndSettle();
+          expect(find.text('M/d/y'), findsNWidgets(2));
+          expect(calendarIconFinder, findsNWidgets(2));
+          expect(switchFinder, findsNWidgets(2));
+          expect(timeIconFinder, findsNothing);
+          await widgetTester.tap(calendarIconFinder.at(0));
+          await widgetTester.pumpAndSettle();
 
-            final start = DateTime.now();
-            final startTime =
-                '${now.hour == 0 ? 12 : now.hour > 12 ? now.hour - 12 : now.hour}'
-                ':${start.minute < 10 ? 0 : ''}${start.minute}'
-                ' ${start.hour > 12 ? 'PM' : 'AM'}';
-            expect(
-              (widgetTester.widget(find.byType(TextFormField).at(2))
-                      as TextFormField)
-                  .initialValue,
-              startTime,
-            );
-            expect(timeIconFinder, findsOneWidget);
+          const pickingStartDate = 1;
+          await widgetTester.tap(find.text('$pickingStartDate'));
+          await widgetTester.tap(find.text('OK'));
+          await widgetTester.pumpAndSettle();
 
-            await widgetTester.tap(calendarIconFinder.at(1));
-            await widgetTester.pumpAndSettle();
+          final startDate = '${now.month}/$pickingStartDate/${now.year}';
+          expect(
+            (widgetTester.widget(find.byType(TextFormField).at(1))
+                    as TextFormField)
+                .initialValue,
+            startDate,
+          );
+          expect(timeIconFinder, findsNothing);
 
-            await widgetTester.tap(find.byIcon(Icons.chevron_right).at(0));
-            await widgetTester.pumpAndSettle();
+          await widgetTester.tap(switchFinder.at(0));
+          await widgetTester.pumpAndSettle();
 
-            const pickingEndDate = 28;
-            await widgetTester.tap(find.text('$pickingEndDate'));
-            await widgetTester.tap(find.text('OK'));
-            await widgetTester.pumpAndSettle();
+          await widgetTester.tap(find.text('OK'));
+          await widgetTester.pumpAndSettle();
 
-            final endDate = '${now.month + 1}/$pickingEndDate/${now.year}';
-            expect(
-              (widgetTester.widget(find.byType(TextFormField).at(3))
-                      as TextFormField)
-                  .initialValue,
-              endDate,
-            );
+          final start = DateTime.now();
+          final startTime =
+              '${now.hour == 0 ? 12 : now.hour > 12 ? now.hour - 12 : now.hour}'
+              ':${start.minute < 10 ? 0 : ''}${start.minute}'
+              ' ${start.hour > 12 ? 'PM' : 'AM'}';
+          expect(
+            (widgetTester.widget(find.byType(TextFormField).at(2))
+                    as TextFormField)
+                .initialValue,
+            startTime,
+          );
+          expect(timeIconFinder, findsOneWidget);
 
-            await widgetTester.enterText(
-              memNameTextFormFieldFinder,
-              'Task scenario: Set Period. - entering mem name ',
-            );
-            await widgetTester.tap(saveMemFabFinder);
-            await widgetTester.pumpAndSettle();
+          await widgetTester.tap(calendarIconFinder.at(1));
+          await widgetTester.pumpAndSettle();
 
-            await widgetTester.pageBack();
-            await widgetTester.pumpAndSettle();
-          },
-        );
-      },
-    );
+          await widgetTester.tap(find.byIcon(Icons.chevron_right).at(0));
+          await widgetTester.pumpAndSettle();
+
+          const pickingEndDate = 28;
+          await widgetTester.tap(find.text('$pickingEndDate'));
+          await widgetTester.tap(find.text('OK'));
+          await widgetTester.pumpAndSettle();
+
+          final endDate = '${now.month + 1}/$pickingEndDate/${now.year}';
+          expect(
+            (widgetTester.widget(find.byType(TextFormField).at(3))
+                    as TextFormField)
+                .initialValue,
+            endDate,
+          );
+
+          await widgetTester.enterText(
+            memNameTextFormFieldFinder,
+            '$scenarioName: Set Period. - entering mem name ',
+          );
+          await widgetTester.tap(saveMemFabFinder);
+          await widgetTester.pumpAndSettle();
+
+          await widgetTester.pageBack();
+          await widgetTester.pumpAndSettle();
+        },
+      );
+    });
 
 final newMemFabFinder = find.byIcon(Icons.add);
 final calendarIconFinder = find.byIcon(Icons.calendar_month);
