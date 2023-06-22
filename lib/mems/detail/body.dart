6e83c3fc740bf0f476a465f6481ecb520be6ff0d
zin- <noyu718@gmail.com>
2023-06-22 14:10:47 +0900

    add `MemDetailBodyV2`
    wip refactoring `MemDetailBody`
    
    wip refactoring `MemDetailPage`
    
    wip #166

--- lib/mems/detail/body.dart
+++ lib/mems/detail/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:mem/logger/i/api.dart';
+import 'package:mem/core/mem.dart';
 import 'package:mem/mems/mem_done_checkbox.dart';
 import 'package:mem/mems/detail/states.dart';
 import 'package:mem/mems/mem_name.dart';
@@ -8,47 +8,54 @@ import 'package:mem/mems/mem_period.dart';
 
 import 'mem_items_view.dart';
 
-class MemDetailBody extends StatelessWidget {
+class MemDetailBody extends ConsumerWidget {
   final int? _memId;
 
-  const MemDetailBody(this._memId, {Key? key}) : super(key: key);
+  const MemDetailBody(this._memId, {super.key});
 
   @override
-  Widget build(BuildContext context) => v(
-        {'_memId': _memId},
-        () {
-          return SingleChildScrollView(
-            child: Consumer(
-              builder: (context, ref, child) {
-                final editingMem = ref.watch(editingMemProvider(_memId));
+  Widget build(BuildContext context, WidgetRef ref) {
+    final editingMem = ref.watch(editingMemProvider(_memId));
 
-                return Column(
-                  children: [
-                    MemNameTextFormField(
-                      editingMem.name,
-                      editingMem.id,
-                      (value) => ref
-                          .read(editingMemProvider(_memId).notifier)
-                          .updatedBy(
-                            editingMem.copied()..name = value,
-                          ),
-                    ),
-                    MemDoneCheckbox(
-                      editingMem,
-                      (value) => ref
-                          .read(editingMemProvider(_memId).notifier)
-                          .updatedBy(
-                            editingMem.copied()
-                              ..doneAt = value == true ? DateTime.now() : null,
-                          ),
-                    ),
-                    MemPeriodTextFormFields(_memId),
-                    MemItemsView(_memId),
-                  ],
-                );
-              },
+    return _MemDetailBodyComponent(
+      editingMem,
+      (value) => ref.read(editingMemProvider(_memId).notifier).updatedBy(
+            editingMem.copied()..name = value,
+          ),
+      (value) => ref.read(editingMemProvider(_memId).notifier).updatedBy(
+            editingMem.copied()..doneAt = value == true ? DateTime.now() : null,
+          ),
+    );
+  }
+}
+
+class _MemDetailBodyComponent extends StatelessWidget {
+  final Mem _mem;
+  final Function(String memName) _onMemNameChanged;
+  final Function(bool? memDone) _onMemDoneChanged;
+
+  const _MemDetailBodyComponent(
+    this._mem,
+    this._onMemNameChanged,
+    this._onMemDoneChanged,
+  );
+
+  @override
+  Widget build(BuildContext context) => SingleChildScrollView(
+        child: Column(
+          children: [
+            MemNameTextFormField(
+              _mem.name,
+              _mem.id,
+              _onMemNameChanged,
             ),
-          );
-        },
+            MemDoneCheckbox(
+              _mem,
+              _onMemDoneChanged,
+            ),
+            MemPeriodTextFormFields(_mem.id),
+            MemItemsView(_mem.id),
+          ],
+        ),
       );
 }
