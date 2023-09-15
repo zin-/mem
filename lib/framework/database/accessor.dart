10d58fa8c2bf0bfef4d76f873fb83afd795d849b
zin- <noyu718@gmail.com>
2023-09-15 09:55:16 +0900

    refactor: `isOpened()`
    wip #209

--- lib/framework/database/accessor.dart
+++ lib/framework/database/accessor.dart
@@ -9,28 +9,26 @@ class DatabaseAccessor {
   Database get nativeDatabase => _nativeDatabase;
 
   Future<bool> isOpened() => v(
-        () async {
-          if (_nativeDatabase.isOpen) {
-            try {
-              return await _nativeDatabase.getVersion().then((value) => true);
-            } on DatabaseException catch (e) {
-              final exceptionMessage = e.toString();
-              // nativeDatabaseがcloseされずにdeleteされた場合に以下のエラーになる
-              if (exceptionMessage.startsWith(
-                    "DatabaseException(database_closed ",
-                  ) ||
-                  exceptionMessage.startsWith(
-                    "SqfliteFfiException(error, Bad state: This database has already been closed})",
-                  )) {
-                warn(e);
-                await _nativeDatabase.close();
-              } else {
-                rethrow;
-              }
-            }
-          }
-          return false;
-        },
+        () async =>
+            _nativeDatabase.isOpen &&
+            await _nativeDatabase.getVersion().then((value) => true).catchError(
+              (e, stackTrace) async {
+                final exceptionMessage = e.toString();
+                // nativeDatabaseがcloseされずにdeleteされた場合に以下のエラーになる
+                if (exceptionMessage.startsWith(
+                      "DatabaseException(database_closed ",
+                    ) ||
+                    exceptionMessage.startsWith(
+                      "SqfliteFfiException(error, Bad state: This database has already been closed})",
+                    )) {
+                  warn(e);
+                  await _nativeDatabase.close();
+                } else {
+                  throw e;
+                }
+                return false;
+              },
+            ),
       );
 
   DatabaseAccessor(this._nativeDatabase);
