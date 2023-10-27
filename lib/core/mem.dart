4bc645b9741bfdc880435cfb0de4a292404a8973
zin- <noyu718@gmail.com>
2023-10-28 00:48:10 +0900

    refactor: drop `MemV1` wip #269

--- lib/core/mem.dart
+++ lib/core/mem.dart
@@ -2,8 +2,6 @@ import 'package:mem/core/date_and_time/date_and_time_period.dart';
 import 'package:mem/framework/repository/entity.dart';
 import 'package:mem/framework/repository/database_tuple_entity.dart';
 
-import 'entity_value.dart';
-
 class MemV2 extends Entity {
   final String name;
   final DateTime? doneAt;
@@ -23,14 +21,6 @@ class MemV2 extends Entity {
         doneAt == null ? this.doneAt : doneAt(),
         period == null ? this.period : period(),
       );
-
-  factory MemV2.fromV1(Mem v1) => v1.isSaved()
-      ? SavedMemV2.fromV1(v1)
-      : MemV2(
-          v1.name,
-          v1.doneAt,
-          v1.period,
-        );
 }
 
 class SavedMemV2<I> extends MemV2 with SavedDatabaseTupleMixin<I> {
@@ -47,57 +37,4 @@ class SavedMemV2<I> extends MemV2 with SavedDatabaseTupleMixin<I> {
         doneAt == null ? this.doneAt : doneAt(),
         period == null ? this.period : period(),
       )..copiedFrom(this);
-
-  factory SavedMemV2.fromV1(Mem v1) => SavedMemV2(
-        v1.name,
-        v1.doneAt,
-        v1.period,
-      )
-        ..id = v1.id as I
-        ..createdAt = v1.createdAt as DateTime
-        ..updatedAt = v1.updatedAt
-        ..archivedAt = v1.archivedAt;
-}
-
-class Mem extends EntityValue {
-  String name;
-  DateTime? doneAt;
-  DateAndTimePeriod? period;
-
-  Mem({
-    required this.name,
-    this.doneAt,
-    this.period,
-    int? id,
-    DateTime? createdAt,
-    DateTime? updatedAt,
-    DateTime? archivedAt,
-  }) : super(
-          id: id,
-          createdAt: createdAt,
-          updatedAt: updatedAt,
-          archivedAt: archivedAt,
-        );
-
-  bool isDone() => doneAt != null;
-
-  @override
-  String toString() =>
-      {
-        'name': name,
-        'doneAt': doneAt,
-        'period': period,
-      }.toString() +
-      super.toString();
-
-  // FIXME エレガントじゃない
-  Mem copied() => Mem(
-        name: name,
-        doneAt: doneAt,
-        period: period,
-        id: id,
-        createdAt: createdAt,
-        updatedAt: updatedAt,
-        archivedAt: archivedAt,
-      );
 }
