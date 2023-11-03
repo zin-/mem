bdff4cd30e2428bb006753db1e39bbba46d09479
zin- <noyu718@gmail.com>
2023-11-03 10:06:21 +0900

    refactor: drop ActV1 wip refactor: replace Act wip #269

--- lib/core/act.dart
+++ lib/core/act.dart
@@ -1,4 +1,3 @@
-import 'package:mem/core/entity_value.dart';
 import 'package:mem/framework/repository/entity.dart';
 import 'package:mem/framework/repository/database_tuple_entity.dart';
 
@@ -22,38 +21,4 @@ class SavedActV2<I> extends ActV2 with SavedDatabaseTupleMixin<I> {
         ..createdAt = createdAt
         ..updatedAt = updatedAt
         ..archivedAt = archivedAt;
-}
-
-class Act extends EntityValue {
-  final int memId;
-  final DateAndTimePeriod period;
-
-  @override
-  int? get id => super.id;
-
-  Act(
-    this.memId,
-    this.period, {
-    super.id,
-    super.createdAt,
-    super.updatedAt,
-    super.archivedAt,
-  });
-
-  Map<String, dynamic> _toMap() => {
-        'memId': memId,
-        'period': period,
-      };
-
-  @override
-  String toString() => _toMap().toString() + super.toString();
-
-  Act copiedWith(DateAndTimePeriod period) => Act(
-        memId,
-        period,
-        id: id,
-        createdAt: createdAt,
-        updatedAt: updatedAt,
-        archivedAt: archivedAt,
-      );
 }
