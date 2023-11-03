dea2ece7230aaa50d332c459ea817d0d03f3ffc5
zin- <noyu718@gmail.com>
2023-11-03 12:28:01 +0900

    refactor: drop MemItem V1 wip refactor: replace MemItem wip #269

--- lib/core/mem_item.dart
+++ lib/core/mem_item.dart
@@ -1,8 +1,6 @@
 import 'package:mem/framework/repository/entity.dart';
 import 'package:mem/framework/repository/database_tuple_entity.dart';
 
-import 'entity_value.dart';
-
 enum MemItemType {
   memo,
 }
@@ -24,20 +22,6 @@ class MemItemV2 extends Entity {
         type,
         value == null ? this.value : value(),
       );
-
-  MemItem toV1() => MemItem(
-        type: type,
-        memId: memId,
-        value: value,
-      );
-
-  factory MemItemV2.fromV1(MemItem v1) => v1.isSaved()
-      ? SavedMemItemV2.fromV1(v1)
-      : MemItemV2(
-          v1.memId,
-          v1.type,
-          v1.value,
-        );
 }
 
 class SavedMemItemV2<I> extends MemItemV2 with SavedDatabaseTupleMixin<I> {
@@ -56,55 +40,4 @@ class SavedMemItemV2<I> extends MemItemV2 with SavedDatabaseTupleMixin<I> {
         type,
         value == null ? this.value : value(),
       )..copiedFrom(this);
-
-  @override
-  MemItem toV1() => MemItem(
-        type: type,
-        memId: memId,
-        value: value,
-        id: id as int,
-        createdAt: createdAt,
-        updatedAt: updatedAt,
-        archivedAt: archivedAt,
-      );
-
-  factory SavedMemItemV2.fromV1(MemItem v1) => SavedMemItemV2(
-        v1.memId as int,
-        v1.type,
-        v1.value,
-      )
-        ..id = v1.id as I
-        ..createdAt = v1.createdAt as DateTime
-        ..updatedAt = v1.updatedAt
-        ..archivedAt = v1.archivedAt;
-}
-
-class MemItem extends EntityValue {
-  int? memId;
-  final MemItemType type;
-  dynamic value;
-
-  MemItem({
-    this.memId,
-    required this.type,
-    this.value,
-    int? id,
-    DateTime? createdAt,
-    DateTime? updatedAt,
-    DateTime? archivedAt,
-  }) : super(
-          id: id,
-          createdAt: createdAt,
-          updatedAt: updatedAt,
-          archivedAt: archivedAt,
-        );
-
-  @override
-  String toString() =>
-      {
-        'memId': memId,
-        'MemItemType': type,
-        'value': value,
-      }.toString() +
-      super.toString();
 }
