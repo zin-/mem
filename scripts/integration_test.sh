58f1df922268bf4f14562cadd8c873f711a6e41c
zin- <noyu718@gmail.com>
2023-11-10 09:40:13 +0900

    refactor: clean up wip #153

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
-# もしCI環境なら引数を求める。
 if [ $# -ne 1 ] && [ "$CI" = "true" ]; then
   echo "引数にデバイス名を指定してください。"
   exit 1
 fi
 
-#ディレクトリ移動
 cd "$(dirname "$0")/.." || exit 1
 
 if [ "$CI" = true ]; then
-  # CI環境のテスト実行
   flutter test integration_test --dart-define=CICD=true --coverage --machine -d "$1" | tee >(grep "^{" | grep "}$" >temp_report.log)
 else
   if [ $# -eq 1 ]; then
-    #デバイス名が設定されているなら
     flutter test integration_test --coverage -d "$1"
   else
-    #デバイス名が設定されていないなら
     flutter test integration_test --coverage
   fi
-  #カバレッジの出力
   genhtml coverage/lcov.info -o coverage/integration_html >/dev/null
-  #開く
   open "file://$(pwd)/coverage/integration_html/index.html"
 fi
