e3b71ffb219249d09657ce2f65395a89dc443a95
zin- <noyu718@gmail.com>
2023-12-13 10:08:23 +0900

    refactor: shorten wip #274

--- lib/components/l10n.dart
+++ lib/components/l10n.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-AppLocalizations buildL10n([BuildContext? context]) {
-  AppLocalizations? tmp;
-
-  if (context != null) tmp = AppLocalizations.of(context);
-
-  return tmp ?? lookupAppLocalizations(const Locale('en'));
-}
+AppLocalizations buildL10n([BuildContext? context]) =>
+    (context != null ? AppLocalizations.of(context) : null) ??
+    lookupAppLocalizations(const Locale('en'));
