cc870b795ce8a9c6beaf3d6dc8c0322ea8fb11c4
zin- <noyu718@gmail.com>
2023-12-28 13:43:49 +0900

    add: Start of day wip add: SettingsPage wip #276

--- lib/settings/page.dart
+++ lib/settings/page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mem/components/l10n.dart';
 import 'package:mem/logger/log_service.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -8,7 +10,41 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) => d(
         () {
           // TODO: implement build
-          throw UnimplementedError();
+          final l10n = buildL10n(context);
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(l10n.settingsPageTitle),
+            ),
+            body: SafeArea(
+              child: SettingsList(
+                sections: [
+                  SettingsSection(
+                    tiles: [
+                      SettingsTile.navigation(
+                        leading: const Icon(Icons.start),
+                        title: Text(l10n.start_of_day_label),
+                        onPressed: (context) => d(
+                          () async {
+                            final picked = await showTimePicker(
+                              context: context,
+                              initialTime: TimeOfDay.now(),
+                            );
+                            // TODO save to preference
+                            debug(picked);
+                          },
+                        ),
+                        value: Text(
+                          // TODO load from preference
+                          TimeOfDay.now().format(context),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
         },
       );
 }
