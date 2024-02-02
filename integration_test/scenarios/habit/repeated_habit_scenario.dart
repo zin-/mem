af476da0577f616b551ede127670c9eaaaeeca40
zin- <noyu718@gmail.com>
2024-02-02 09:45:46 +0900

    add: test `Repeated habit scenario: Show: on saved.` wip #279

--- integration_test/scenarios/habit/repeated_habit_scenario.dart
+++ integration_test/scenarios/habit/repeated_habit_scenario.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mem/core/mem_notification.dart';
+import 'package:mem/databases/definition.dart';
+import 'package:mem/databases/table_definitions/base.dart';
+import 'package:mem/databases/table_definitions/mem_notifications.dart';
+import 'package:mem/databases/table_definitions/mems.dart';
+import 'package:mem/framework/database/accessor.dart';
 import 'package:mem/mems/detail/notifications_view.dart';
 
 import '../helpers.dart';
@@ -13,25 +19,79 @@ const _scenarioName = 'Repeated habit scenario';
 void testRepeatedHabitScenario() => group(
       ": $_scenarioName",
       () {
-        testWidgets(
-          ": show on new.",
-          (widgetTester) async {
-            await runApplication();
-            await widgetTester.pumpAndSettle();
-            await widgetTester.tap(newMemFabFinder);
-            await widgetTester.pumpAndSettle();
+        group(": Show", () {
+          late final DatabaseAccessor dbA;
+          setUpAll(() async {
+            dbA = await openTestDatabase(databaseDefinition);
+          });
 
-            expect(
-              widgetTester
-                  .widget<TextFormField>(
-                    find.descendant(
-                        of: find.byKey(keyMemRepeatedNotification),
-                        matching: find.byType(TextFormField)),
-                  )
-                  .initialValue,
-              isEmpty,
+          const insertedMemName = "$_scenarioName - inserted - mem - name";
+          late int insertedMemId;
+
+          setUp(() async {
+            await clearAllTestDatabaseRows(databaseDefinition);
+
+            insertedMemId = await dbA.insert(
+              defTableMems,
+              {
+                defColMemsName.name: insertedMemName,
+                defColCreatedAt.name: zeroDate,
+              },
             );
-          },
-        );
+            await dbA.insert(
+              defTableMemNotifications,
+              {
+                defFkMemNotificationsMemId.name: insertedMemId,
+                defColMemNotificationsType.name:
+                    MemNotificationType.repeat.name,
+                defColMemNotificationsTime.name: 1,
+                defColMemNotificationsMessage.name: "never",
+                defColCreatedAt.name: zeroDate,
+              },
+            );
+          });
+
+          testWidgets(
+            ": on new.",
+            (widgetTester) async {
+              await runApplication();
+              await widgetTester.pumpAndSettle();
+              await widgetTester.tap(newMemFabFinder);
+              await widgetTester.pumpAndSettle();
+
+              expect(
+                widgetTester
+                    .widget<TextFormField>(
+                      find.descendant(
+                          of: find.byKey(keyMemRepeatedNotification),
+                          matching: find.byType(TextFormField)),
+                    )
+                    .initialValue,
+                isEmpty,
+              );
+            },
+          );
+
+          testWidgets(
+            ": on saved.",
+            (widgetTester) async {
+              await runApplication();
+              await widgetTester.pumpAndSettle();
+              await widgetTester.tap(find.text(insertedMemName));
+              await widgetTester.pumpAndSettle();
+
+              expect(
+                widgetTester
+                    .widget<TextFormField>(
+                      find.descendant(
+                          of: find.byKey(keyMemRepeatedNotification),
+                          matching: find.byType(TextFormField)),
+                    )
+                    .initialValue,
+                "12:00 AM",
+              );
+            },
+          );
+        });
       },
     );
