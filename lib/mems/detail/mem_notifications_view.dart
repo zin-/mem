b00c443f0058e8eb8482052b8ea3e68916a4d885
zin- <noyu718@gmail.com>
2024-02-13 09:42:45 +0900

    refactor: clean up wip add: MemNotificationsPage wip add: repeat every n days wip #279
    TODO drop NotificationsView
    TODO implements MemNotificationsPage

--- lib/mems/detail/mem_notifications_view.dart
+++ lib/mems/detail/mem_notifications_view.dart
@@ -22,23 +22,20 @@ class MemNotificationsView extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) => v(
         () => _MemNotificationsView(
           _memId,
-          ref.read(memNotificationsByMemIdProvider(_memId).select(
-            (value) => value.where((element) => element.isEnabled()).isNotEmpty,
-          )),
           ref.read(memNotificationsByMemIdProvider(_memId)),
         ),
-        {"_memId": _memId},
+        {
+          "_memId": _memId,
+        },
       );
 }
 
 class _MemNotificationsView extends StatelessWidget {
   final int? _memId;
-  final bool _hasEnabledNotifications;
   final List<MemNotification> _memNotifications;
 
   const _MemNotificationsView(
     this._memId,
-    this._hasEnabledNotifications,
     this._memNotifications,
   ) : super(key: keyMemNotificationsView);
 
@@ -47,32 +44,34 @@ class _MemNotificationsView extends StatelessWidget {
         () {
           final l10n = buildL10n(context);
 
+          final hasEnabledNotifications = _memNotifications
+              .where((element) => element.isEnabled())
+              .isNotEmpty;
+          final text = _memNotifications
+              .where((element) => element.isEnabled())
+              .map((e) {
+            switch (e.type) {
+              case MemNotificationType.repeat:
+                return l10n.repeated_notification_text(TimeOfDay.fromDateTime(
+                  DateAndTime(0, 0, 0, 0, 0, e.time),
+                ).format(context));
+              case MemNotificationType.afterActStarted:
+                return l10n.after_act_started_notification_text(
+                    DateFormat(DateFormat.HOUR24_MINUTE)
+                        .format(DateAndTime(0, 0, 0, 0, 0, e.time)));
+            }
+          }).join(", ");
+
           return ListTile(
             contentPadding: EdgeInsets.zero,
             leading: Icon(
               Icons.notifications,
-              color: _hasEnabledNotifications ? null : secondaryGreyColor,
+              color: hasEnabledNotifications ? null : secondaryGreyColor,
             ),
             title: Text(
-              _hasEnabledNotifications
-                  ? _memNotifications
-                      .where((element) => element.isEnabled())
-                      .map((e) {
-                      switch (e.type) {
-                        case MemNotificationType.repeat:
-                          return l10n
-                              .repeat_notification(TimeOfDay.fromDateTime(
-                            DateAndTime(0, 0, 0, 0, 0, e.time),
-                          ).format(context));
-                        case MemNotificationType.afterActStarted:
-                          return l10n.after_act_started_notification_text(
-                              DateFormat(DateFormat.HOUR24_MINUTE)
-                                  .format(DateAndTime(0, 0, 0, 0, 0, e.time)));
-                      }
-                    }).join(", ")
-                  : l10n.no_notifications,
+              hasEnabledNotifications ? text : l10n.no_notifications,
               style: TextStyle(
-                color: _hasEnabledNotifications ? null : secondaryGreyColor,
+                color: hasEnabledNotifications ? null : secondaryGreyColor,
               ),
             ),
             trailing: IconButton(
@@ -85,9 +84,9 @@ class _MemNotificationsView extends StatelessWidget {
                         reverseTransitionDuration: defaultTransitionDuration,
                       ))),
               icon: Icon(
-                _hasEnabledNotifications ? Icons.edit : Icons.notification_add,
+                hasEnabledNotifications ? Icons.edit : Icons.notification_add,
               ),
-              tooltip: _hasEnabledNotifications
+              tooltip: hasEnabledNotifications
                   ? l10n.edit_notification
                   : l10n.add_notification,
             ),
@@ -95,7 +94,6 @@ class _MemNotificationsView extends StatelessWidget {
         },
         {
           "_memId": _memId,
-          "_hasEnabledNotifications": _hasEnabledNotifications,
           "_memNotifications": _memNotifications,
         },
       );
