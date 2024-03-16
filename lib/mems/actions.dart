1615fccb88f1c2b40dd79a04c962fe0f9e209078
zin- <noyu718@gmail.com>
2024-03-17 08:26:37 +0900

    fix: test `Memo detail scenario: Remove: undo.` wip #305

--- lib/mems/actions.dart
+++ lib/mems/actions.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mem/logger/log_service.dart';
+import 'package:mem/mems/mem_item.dart';
 import 'package:mem/mems/mem_service.dart';
 import 'package:mem/mems/states.dart';
 import 'package:mem/repositories/mem.dart';
@@ -17,7 +18,13 @@ final undoRemoveMem = FutureProvider.autoDispose.family<void, int>(
         if (removeUndoneMem is SavedMem) {
           ref.read(memsProvider.notifier).add(removeUndoneMem);
           for (var element in removeUndone.memItems) {
-            ref.read(memItemsProvider.notifier).add(element);
+            ref.read(memItemsProvider.notifier).upsertAll(
+              [element],
+              (current, updating) =>
+                  current is SavedMemItem &&
+                  updating is SavedMemItem &&
+                  current.id == updating.id,
+            );
           }
         }
       }
