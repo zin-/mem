c4ac1ea4a3fbd7a45ffe77341fb5d673302773e8
zin- <noyu718@gmail.com>
2024-04-10 12:30:41 +0900

    refactor: drop `_ActListIF` wip #72

--- lib/acts/list/act_list.dart
+++ lib/acts/list/act_list.dart
@@ -3,10 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:mem/acts/actions.dart';
-import 'package:mem/acts/list/app_bar.dart';
-import 'package:mem/acts/list/states.dart';
-import 'package:mem/acts/list/sub_header.dart';
-import 'package:mem/acts/list/item/total_act_time_item.dart';
 import 'package:mem/acts/states.dart';
 import 'package:mem/components/async_value_view.dart';
 import 'package:mem/components/l10n.dart';
@@ -17,7 +13,11 @@ import 'package:mem/mems/states.dart';
 import 'package:mem/repositories/mem.dart';
 import 'package:mem/values/constants.dart';
 
+import 'app_bar.dart';
+import 'item/total_act_time_item.dart';
 import 'item/view.dart';
+import 'states.dart';
+import 'sub_header.dart';
 
 class ActList extends ConsumerWidget {
   final int? _memId;
@@ -34,28 +34,25 @@ class ActList extends ConsumerWidget {
         () => AsyncValueView(
           loadActList(_memId),
           (loaded) => _ActList(
-            _ActListIF(
-              _memId == null
-                  ? buildL10n(context).defaultActListPageTitle
-                  : ref.read(memByMemIdProvider(_memId))?.name ??
-                      somethingWrong,
-              ref.watch(dateViewProvider),
-              (bool changed) =>
-                  ref.read(dateViewProvider.notifier).updatedBy(changed),
-              ref.watch(timeViewProvider),
-              (bool changed) =>
-                  ref.read(timeViewProvider.notifier).updatedBy(changed),
-              ref.watch(actListProvider(_memId)) ?? [],
-              (_memId == null ? ref.watch(memListProvider) : []),
-              _scrollController,
-            ),
+            _memId == null
+                ? buildL10n(context).defaultActListPageTitle
+                : ref.read(memByMemIdProvider(_memId))?.name ?? somethingWrong,
+            ref.watch(dateViewProvider),
+            (bool changed) =>
+                ref.read(dateViewProvider.notifier).updatedBy(changed),
+            ref.watch(timeViewProvider),
+            (bool changed) =>
+                ref.read(timeViewProvider.notifier).updatedBy(changed),
+            ref.watch(actListProvider(_memId)) ?? [],
+            (_memId == null ? ref.watch(memListProvider) : []),
+            _scrollController,
           ),
         ),
         {"_memId": _memId},
       );
 }
 
-class _ActListIF {
+class _ActList extends StatelessWidget {
   final String _title;
   final bool _isDateView;
   final void Function(bool changed) _changeDateViewMode;
@@ -65,7 +62,7 @@ class _ActListIF {
   final List<SavedMem> _memList;
   final ScrollController? _scrollController;
 
-  _ActListIF(
+  const _ActList(
     this._title,
     this._isDateView,
     this._changeDateViewMode,
@@ -76,60 +73,49 @@ class _ActListIF {
     this._scrollController,
   );
 
-  Map<String, dynamic> _toMap() => {
-        "_title": _title,
-        "_isDateView": _isDateView,
-        "_isTimeView": _isTimeView,
-        "_actList": _actList,
-        "_memList": _memList,
-      };
-
-  @override
-  String toString() => _toMap().toString();
-}
-
-class _ActList extends StatelessWidget {
-  final _ActListIF _actListIF;
-
-  const _ActList(this._actListIF);
-
   @override
   Widget build(BuildContext context) => v(
         () => CustomScrollView(
-          controller: _actListIF._scrollController,
+          controller: _scrollController,
           slivers: [
             ActListAppBar(
-              _actListIF._title,
-              _actListIF._isDateView,
-              _actListIF._changeDateViewMode,
-              _actListIF._isTimeView,
-              _actListIF._changeTimeViewMode,
+              _title,
+              _isDateView,
+              _changeDateViewMode,
+              _isTimeView,
+              _changeTimeViewMode,
             ),
-            ..._actListIF._actList
+            ..._actList
                 .groupListsBy(
                   (element) => DateTime(
                     element.period.start!.year,
                     element.period.start!.month,
-                    _actListIF._isDateView ? element.period.start!.day : 1,
+                    _isDateView ? element.period.start!.day : 1,
                   ),
                 )
                 .entries
                 .map(
                   (e) => SliverStickyHeader(
-                    header: ActListSubHeader(e, _actListIF._isDateView),
+                    header: ActListSubHeader(e, _isDateView),
                     sliver: SliverList(
-                      delegate: _actListIF._isTimeView
+                      delegate: _isTimeView
                           ? _SummaryActListItem(
                               e.value.groupListsBy((element) => element.memId),
-                              _actListIF._memList,
+                              _memList,
                             )
-                          : _SimpleActListItem(e.value, _actListIF._memList),
+                          : _SimpleActListItem(e.value, _memList),
                     ),
                   ),
                 )
           ],
         ),
-        _actListIF,
+        {
+          "_title": _title,
+          "_isDateView": _isDateView,
+          "_isTimeView": _isTimeView,
+          "_actList": _actList,
+          "_memList": _memList,
+        },
       );
 }
 
