d551b56dcbce5b7f5226f7bc73a667f99a6e8839
zin- <noyu718@gmail.com>
2024-05-13 01:43:41 +0900

    modify: 分けすぎてもあまり良くなさそうなのでframeworkとsettingsだけ分割してみる wip fix #313

---
+++ integration_test/scenarios/app_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:mem/logger/log.dart';
+import 'package:mem/logger/log_service.dart';
+
+import 'edge_scenario.dart';
+import 'memo/memo_test.dart' as memo_test;
+import 'habit/habit_test.dart' as habit_test;
+import 'notification_scenario.dart';
+import 'task_scenario.dart';
+import 'todo_scenario.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  const onCICD = bool.fromEnvironment('CICD', defaultValue: false);
+  LogService.initialize(
+    Level.verbose,
+    const bool.fromEnvironment('CICD', defaultValue: false),
+  );
+  info({'onCICD': onCICD});
+
+  group('Scenario test', () {
+    memo_test.main();
+    habit_test.main();
+
+    testTodoScenario();
+    testTaskScenario();
+
+    testNotificationScenario();
+    testEdgeScenario();
+  });
+}
