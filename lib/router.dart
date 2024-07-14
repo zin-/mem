54448a7b863896ef0cfedfe14df1c358a8c4f317
zin- <noyu718@gmail.com>
2024-07-14 12:16:43 +0900

    use: `go_router` wip #333

---
+++ lib/router.dart
@@ -0,0 +1,34 @@
+import 'package:go_router/go_router.dart';
+import 'package:mem/act_counter/act_counter_configure.dart';
+import 'package:mem/home_screen.dart';
+import 'package:mem/mems/detail/page.dart';
+
+const _memIdKey = 'memId';
+
+const _homePath = '/';
+const _memDetailPath = 'mems/:$_memIdKey';
+const newActCountersPath = '${_homePath}act_counters/new';
+
+String buildMemDetailPath(int memId) => '${_homePath}mems/$memId';
+
+GoRouter buildRouter([String? initialPath = _homePath]) => GoRouter(
+      initialLocation: initialPath,
+      routes: [
+        GoRoute(
+          path: _homePath,
+          builder: (_, __) => HomeScreen(),
+          routes: [
+            GoRoute(
+              path: _memDetailPath,
+              builder: (_, state) => MemDetailPage(
+                int.parse(state.pathParameters[_memIdKey]!),
+              ),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: newActCountersPath,
+          builder: (_, __) => const ActCounterConfigure(),
+        ),
+      ],
+    );
