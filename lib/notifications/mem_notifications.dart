f276da2c4b6f6f9fc52714aba98b6028d10f00ac
zin- <noyu718@gmail.com>
2024-08-01 09:46:23 +0900

    refactor: drop useless whereType
    wip #373

--- lib/notifications/mem_notifications.dart
+++ lib/notifications/mem_notifications.dart
@@ -23,7 +23,6 @@ class MemNotifications {
   ) =>
       v(
         () => memNotifications
-                .whereType<SavedMemNotification>()
                 .where(
                   (e) =>
                       e.isEnabled() &&
