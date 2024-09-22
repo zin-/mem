d89d7ee6396e45333bb89f5f390bcdb05cab42bb
zin- <noyu718@gmail.com>
2024-09-22 15:44:00 +0900

    wip fix: failed test `Scenario test Memo scenario Memo detail scenario : Save: create.`

--- integration_test/scenarios/memo/memo_test.dart
+++ integration_test/scenarios/memo/memo_test.dart
@@ -5,17 +5,14 @@ import 'package:mem/logger/log_service.dart';
 import 'list_scenarios.dart' as list_scenarios;
 import 'detail_scenarios.dart' as detail_scenarios;
 
-const _scenarioName = "Memo scenario";
+const _scenarioName = 'Memo scenario';
 
-void main() => group(
-      _scenarioName,
-      () {
-        LogService.initialize(
-          Level.verbose,
-          const bool.fromEnvironment('CICD', defaultValue: false),
-        );
+void main() => group(': $_scenarioName', () {
+      LogService.initialize(
+        Level.verbose,
+        const bool.fromEnvironment('CICD', defaultValue: false),
+      );
 
-        list_scenarios.main();
-        detail_scenarios.main();
-      },
-    );
+      list_scenarios.main();
+      detail_scenarios.main();
+    });
