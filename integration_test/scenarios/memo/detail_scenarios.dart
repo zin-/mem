aa514833aa954d0d66a045062d94a3157f5d536a
zin- <noyu718@gmail.com>
2024-09-23 11:43:48 +0900

    refactor: move from `components`

--- integration_test/scenarios/memo/detail_scenarios.dart
+++ integration_test/scenarios/memo/detail_scenarios.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:mem/components/mem/mem_name.dart';
+import 'package:mem/mems/mem_name.dart';
 import 'package:mem/mems/mem_item.dart';
 import 'package:mem/databases/definition.dart';
 import 'package:mem/databases/table_definitions/base.dart';
