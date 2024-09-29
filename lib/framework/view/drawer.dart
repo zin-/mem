f9ac553084d4e3c9f4eda3ff673abb5f581099bd
zin- <noyu718@gmail.com>
2024-09-29 15:01:19 +0900

    refactor: `ApplicationDrawer`

--- lib/framework/view/drawer.dart
+++ lib/framework/view/drawer.dart
@@ -1,29 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:mem/l10n/l10n.dart';
 import 'package:mem/logger/log_service.dart';
 import 'package:mem/settings/page.dart';
 
-class ApplicationDrawer extends Drawer {
-  ApplicationDrawer(
-    AppLocalizations l10n, {
-    super.key,
-  }) : super(
-          child: ListView.builder(
-            itemCount: 1,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: const Icon(Icons.settings),
-                title: Text(l10n.settingsPageTitle),
-                onTap: () => v(
-                  () => Navigator.of(context).push(
-                    PageRouteBuilder(
-                      pageBuilder: (context, animation, secondaryAnimation) =>
-                          const SettingsPage(),
-                    ),
-                  ),
-                ),
-              );
-            },
+class _DrawerItem {
+  final IconData _icon;
+  final String Function(AppLocalizations) _titleBuilder;
+  final Function() Function(BuildContext) _onTapBuilder;
+
+  _DrawerItem(this._icon, this._titleBuilder, this._onTapBuilder);
+}
+
+class ApplicationDrawer extends StatelessWidget {
+  final _itemList = [
+    _DrawerItem(
+      Icons.settings,
+      (AppLocalizations l10n) => l10n.settingsPageTitle,
+      (BuildContext context) => () => v(
+            () => Navigator.of(context).push(
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    const SettingsPage(),
+              ),
+            ),
           ),
-        );
+    ),
+  ];
+
+  ApplicationDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) => v(
+        () {
+          final l10n = buildL10n(context);
+
+          return Drawer(
+            child: ListView.builder(
+              itemCount: _itemList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(_itemList[index]._icon),
+                  title: Text(_itemList[index]._titleBuilder(l10n)),
+                  onTap: _itemList[index]._onTapBuilder(context),
+                );
+              },
+            ),
+          );
+        },
+        {
+          'context': context,
+          '_itemList': _itemList,
+        },
+      );
 }
