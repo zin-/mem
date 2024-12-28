ce6b7f7cf674c0d5a000279e7413f58e31dd1c5d
zin- <noyu718@gmail.com>
2024-12-28 19:50:59 +0900

    coverage

--- lib/mems/mem_item_repository.dart
+++ lib/mems/mem_item_repository.dart
@@ -1,11 +1,10 @@
 import 'package:mem/databases/definition.dart';
-import 'package:mem/databases/table_definitions/base.dart';
 import 'package:mem/databases/table_definitions/mem_items.dart';
+import 'package:mem/framework/repository/condition/conditions.dart';
 import 'package:mem/framework/repository/database_tuple_repository.dart';
 import 'package:mem/framework/repository/group_by.dart';
 import 'package:mem/framework/repository/order_by.dart';
 import 'package:mem/logger/log_service.dart';
-import 'package:mem/framework/repository/condition/conditions.dart';
 import 'package:mem/mems/mem_item_entity.dart';
 
 class MemItemRepositoryV2
@@ -88,13 +87,11 @@ class MemItemRepositoryV2
 
   @override
   Future<List<SavedMemItemEntityV2>> waste({
-    int? memId,
     Condition? condition,
   }) =>
       super.waste(
         condition: And(
           [
-            if (memId != null) Equals(defPkId, memId),
             if (condition != null) condition, // coverage:ignore-line
           ],
         ),
