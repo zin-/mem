a61c8e964b3635b7324d79f032998aef47ec3538
zin- <noyu718@gmail.com>
2025-05-13 15:46:20 +0900

    refactor

--- lib/features/targets/target_view.dart
+++ lib/features/targets/target_view.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mem/acts/line_chart/states.dart';
 import 'package:mem/features/targets/target.dart';
-import 'package:mem/features/targets/target_entity.dart';
 import 'package:mem/features/targets/target_states.dart';
-import 'package:mem/mems/detail/states.dart';
-import 'package:mem/mems/transitions.dart';
 import 'package:mem/values/dimens.dart';
-import 'package:mem/values/durations.dart';
 
 class TargetText extends ConsumerWidget {
   final int? _memId;
@@ -17,129 +13,71 @@ class TargetText extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO: implement build
+    final targetEntity = ref.watch(targetStateProvider(_memId));
+    final onTargetTypeChanged =
+        ref.read(targetStateProvider(_memId).notifier).updatedWith;
+
     return ListTile(
       contentPadding: EdgeInsets.zero,
       leading: Icon(Icons.check_circle),
       // 1日20回以下、週40時間以上、1ヶ月に5時間以下みたいな感じ
-      title: Text("Less than 20 times per a day"),
-      trailing: IconButton(
-        onPressed: () => Navigator.of(context).push(
-          PageRouteBuilder(
-            pageBuilder: (context, animation, secondaryAnimation) =>
-                MemDetailTargetScreen(_memId),
-            transitionsBuilder: detailTransitionsBuilder,
-            transitionDuration: defaultTransitionDuration,
-            reverseTransitionDuration: defaultTransitionDuration,
+      title: Flex(
+        direction: Axis.horizontal,
+        spacing: defaultComponentPadding,
+        children: [
+          DropdownButton(
+            value: targetEntity.value.targetType.index,
+            items: TargetType.values
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e.index,
+                    child: Text(e.name),
+                  ),
+                )
+                .toList(),
+            onChanged: (v) => onTargetTypeChanged(
+              targetType: () => TargetType.values[v!],
+            ),
           ),
-        ),
-        icon: Icon(Icons.edit),
-      ),
-    );
-  }
-}
-
-class MemDetailTargetScreen extends ConsumerWidget {
-  final int? _memId;
-
-  const MemDetailTargetScreen(this._memId, {super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
-    final targetEntity = ref.watch(targetStateProvider(_memId));
-
-    return _MemDetailTargetScreen(
-      ref.watch(
-        editingMemByMemIdProvider(_memId).select(
-          (value) => value.value.name,
-        ),
-      ),
-      targetEntity,
-      ref.read(targetStateProvider(_memId).notifier).updatedWith,
-    );
-  }
-}
-
-class _MemDetailTargetScreen extends StatelessWidget {
-  final String _memName;
-  final TargetEntity _targetEntity;
-  final Function({
-    TargetType? Function()? targetType,
-    TargetUnit? Function()? targetUnit,
-    int? Function()? value,
-    Period? Function()? period,
-  }) _onTargetTypeChanged;
-
-  const _MemDetailTargetScreen(
-    this._memName,
-    this._targetEntity,
-    this._onTargetTypeChanged,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(_memName),
-      ),
-      body: Padding(
-        padding: defaultPadding,
-        child: Flex(
-          direction: Axis.vertical,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            DropdownButton(
-              value: _targetEntity.value.targetType.index,
-              items: TargetType.values
-                  .map(
-                    (e) => DropdownMenuItem(
-                      value: e.index,
-                      child: Text(e.name),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (v) => _onTargetTypeChanged(
-                targetType: () => TargetType.values[v!],
-              ),
+          DropdownButton(
+            value: targetEntity.value.targetUnit.index,
+            items: TargetUnit.values
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e.index,
+                    child: Text(e.name),
+                  ),
+                )
+                .toList(),
+            onChanged: (v) => onTargetTypeChanged(
+              targetUnit: () => TargetUnit.values[v!],
             ),
-            DropdownButton(
-              value: _targetEntity.value.targetUnit.index,
-              items: TargetUnit.values
-                  .map(
-                    (e) => DropdownMenuItem(
-                      value: e.index,
-                      child: Text(e.name),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (v) => _onTargetTypeChanged(
-                targetUnit: () => TargetUnit.values[v!],
-              ),
-            ),
-            TextFormField(
-              initialValue: _targetEntity.value.value.toString(),
-              onChanged: (v) => _onTargetTypeChanged(
+          ),
+          Expanded(
+            child: TextFormField(
+              initialValue: targetEntity.value.value.toString(),
+              onChanged: (v) => onTargetTypeChanged(
                 value: () => int.parse(v),
               ),
             ),
-            DropdownButton(
-              // aDayが欲しい
-              value: _targetEntity.value.period.index,
-              items: Period.values
-                  .map(
-                    (e) => DropdownMenuItem(
-                      value: e.index,
-                      child: Text(e.name),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (v) => _onTargetTypeChanged(
-                period: () => Period.values[v!],
-              ),
+          ),
+          Text('/'),
+          DropdownButton(
+            // aDayが欲しい
+            value: targetEntity.value.period.index,
+            items: Period.values
+                .map(
+                  (e) => DropdownMenuItem(
+                    value: e.index,
+                    child: Text(e.name),
+                  ),
+                )
+                .toList(),
+            onChanged: (v) => onTargetTypeChanged(
+              period: () => Period.values[v!],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
