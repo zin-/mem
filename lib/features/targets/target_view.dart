5126912a78e29d4adcf0377176647ed2589225d7
zin- <noyu718@gmail.com>
2025-05-14 10:42:23 +0900

    refactor

--- lib/features/targets/target_view.dart
+++ lib/features/targets/target_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mem/acts/line_chart/states.dart';
 import 'package:mem/features/targets/target.dart';
+import 'package:mem/features/targets/target_entity.dart';
 import 'package:mem/features/targets/target_states.dart';
 import 'package:mem/values/dimens.dart';
 
@@ -14,103 +15,111 @@ class TargetText extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
     final targetEntity = ref.watch(targetStateProvider(_memId));
-    final onTargetTypeChanged =
-        ref.read(targetStateProvider(_memId).notifier).updatedWith;
 
     return ListTile(
       contentPadding: EdgeInsets.zero,
       leading: Icon(Icons.check_circle),
-      // 1日20回以下、週40時間以上、1ヶ月に5時間以下みたいな感じ
       title: switch (targetEntity) {
-        AsyncData(:final value) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      value: value.value.targetType.index,
-                      items: TargetType.values
-                          .map(
-                            (e) => DropdownMenuItem(
-                              value: e.index,
-                              child: Text(e.name),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (v) => onTargetTypeChanged(
-                        targetType: () => TargetType.values[v!],
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: defaultComponentPadding),
-                  Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      value: value.value.targetUnit.index,
-                      items: TargetUnit.values
-                          .map(
-                            (e) => DropdownMenuItem(
-                              value: e.index,
-                              child: Text(e.name),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (v) => onTargetTypeChanged(
-                        targetUnit: () => TargetUnit.values[v!],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: defaultComponentPadding),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      key: keyTargetValue,
-                      initialValue: value.value.value.toString(),
-                      onChanged: (v) => onTargetTypeChanged(
-                        value: () {
-                          final value = int.tryParse(v);
-                          if (value == null) {
-                            return null;
-                          }
-                          return value;
-                        },
-                      ),
-                    ),
-                  ),
-                  SizedBox(width: defaultComponentPadding),
-                  Text('/'),
-                  SizedBox(width: defaultComponentPadding),
-                  Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      value: value.value.period.index,
-                      items: Period.values
-                          .map(
-                            (e) => DropdownMenuItem(
-                              value: e.index,
-                              child: Text(e.name),
-                            ),
-                          )
-                          .toList(),
-                      onChanged: (v) => onTargetTypeChanged(
-                        period: () => Period.values[v!],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+        AsyncData(:final value) => _build(
+            value, ref.read(targetStateProvider(_memId).notifier).updatedWith),
         AsyncError() => const Text('Oops, something unexpected happened'),
         _ => const CircularProgressIndicator(),
       },
     );
   }
+
+  Widget _build(
+    TargetEntity targetEntity,
+    void Function({
+      TargetType? Function()? targetType,
+      TargetUnit? Function()? targetUnit,
+      int? Function()? value,
+      Period? Function()? period,
+    }) onTargetChanged,
+  ) =>
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: DropdownButton(
+                  isExpanded: true,
+                  value: targetEntity.value.targetType.index,
+                  items: TargetType.values
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e.index,
+                          child: Text(e.name),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (v) => onTargetChanged(
+                    targetType: () => TargetType.values[v!],
+                  ),
+                ),
+              ),
+              SizedBox(width: defaultComponentPadding),
+              Expanded(
+                child: DropdownButton(
+                  isExpanded: true,
+                  value: targetEntity.value.targetUnit.index,
+                  items: TargetUnit.values
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e.index,
+                          child: Text(e.name),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (v) => onTargetChanged(
+                    targetUnit: () => TargetUnit.values[v!],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: defaultComponentPadding),
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  key: keyTargetValue,
+                  initialValue: targetEntity.value.value.toString(),
+                  onChanged: (v) => onTargetChanged(
+                    value: () {
+                      final value = int.tryParse(v);
+                      if (value == null) {
+                        return null;
+                      }
+                      return value;
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(width: defaultComponentPadding),
+              Text('/'),
+              SizedBox(width: defaultComponentPadding),
+              Expanded(
+                child: DropdownButton(
+                  isExpanded: true,
+                  value: targetEntity.value.period.index,
+                  items: Period.values
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e.index,
+                          child: Text(e.name),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (v) => onTargetChanged(
+                    period: () => Period.values[v!],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      );
 }
