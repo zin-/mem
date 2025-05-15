6c47000fe7a5d0e2817ef95d65b251c637134819
zin- <noyu718@gmail.com>
2025-05-15 13:32:58 +0900

    feat: add improvement suggestions for TimeTextFormField including keyboard type, validation, and localization

--- lib/framework/date_and_time/time_text_form_field.dart
+++ lib/framework/date_and_time/time_text_form_field.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:mem/framework/date_and_time/seconds_of_time_picker.dart';
 import 'package:mem/logger/log_service.dart';
 
+// TODO: 以下の改善を検討
+// - 数値キーボードの表示（keyboardType: TextInputType.number）
+// - 基本的な数値バリデーションの追加
+// - ヒントテキストの改善（より具体的な例の提示）
+// - エラーメッセージのローカライズ対応
+// - 最小値・最大値のバリデーション追加
+// - エラーメッセージのカスタマイズ機能追加
 class TimeTextFormField extends StatelessWidget {
   final int? secondsOfTime;
   final void Function(int? pickedSecondsOfTime) _onChanged;
