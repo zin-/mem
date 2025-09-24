5f2a41eebb93d96808f754f8ddc37d976dfa297d
zin- <noyu718@gmail.com>
2025-09-25 08:22:14 +0900

    テストシナリオでの変数の整理とコメントアウトされたコードの削除を行い、可読性を向上。新しい変数`insertedMemId2`を追加し、`actStart`の使用を明確化。

--- integration_test/scenarios/habit/act_counter_scenario.dart
+++ integration_test/scenarios/habit/act_counter_scenario.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-// import 'package:mem/features/acts/counter/act_counter_entity.dart';
+import 'package:mem/features/acts/counter/act_counter_entity.dart';
 import 'package:mem/databases/definition.dart';
 import 'package:mem/databases/table_definitions/acts.dart';
 import 'package:mem/databases/table_definitions/base.dart';
@@ -20,7 +20,8 @@ void main() => group(
         const insertedMemName = '$_name: inserted - mem name';
         const insertedMemName2 = '$_name: inserted - mem name - 2';
         late int insertedMemId;
-        // late DateTime actStart;
+        late int insertedMemId2;
+        late DateTime actStart;
 
         late final DatabaseAccessor dbA;
 
@@ -37,7 +38,7 @@ void main() => group(
               defColCreatedAt.name: zeroDate,
             },
           );
-          await dbA.insert(
+          insertedMemId2 = await dbA.insert(
             defTableMems,
             {
               defColMemsName.name: insertedMemName2,
@@ -57,7 +58,7 @@ void main() => group(
             defTableActs,
             {
               defFkActsMemId.name: insertedMemId,
-              defColActsStart.name: DateTime.now(),
+              defColActsStart.name: actStart = DateTime.now(),
               defColActsStartIsAllDay.name: 0,
               defColCreatedAt.name: zeroDate,
             },
@@ -67,75 +68,75 @@ void main() => group(
         testWidgets(
           ': select saved mem.',
           (widgetTester) async {
-            // var initializeCount = 0;
-            // var saveWidgetDataCount = 0;
-            // var updateWidgetCount = 0;
-            // final homeWidgetId = randomInt();
-            // final actCounter =
-            //     ActCounterEntity(insertedMemId, insertedMemName, 1, actStart);
+            var initializeCount = 0;
+            var saveWidgetDataCount = 0;
+            var updateWidgetCount = 0;
+            final homeWidgetId = randomInt();
+            final actCounter =
+                ActCounterEntity(insertedMemId, insertedMemName, 1, actStart);
 
-            // widgetTester.binding.defaultBinaryMessenger
-            //     .setMockMethodCallHandler(
-            //   MethodChannel(actCounter.methodChannelName),
-            //   (message) {
-            //     expect(message.method, actCounter.initializeMethodName);
-            //     expect(message.arguments, null);
+            widgetTester.binding.defaultBinaryMessenger
+                .setMockMethodCallHandler(
+              MethodChannel(actCounter.methodChannelName),
+              (message) {
+                expect(message.method, actCounter.initializeMethodName);
+                expect(message.arguments, null);
 
-            //     initializeCount++;
-            //     return Future.value(homeWidgetId);
-            //   },
-            // );
-            // final saveWidgetDataArgs = {
-            //   0: {
-            //     'id': "memName-$insertedMemId",
-            //     'data': insertedMemName,
-            //   },
-            //   1: {
-            //     'id': "actCount-$insertedMemId",
-            //     // length of inserted acts
-            //     'data': 1,
-            //   },
-            //   2: {
-            //     'id': "lastUpdatedAtSeconds-$insertedMemId",
-            //     'data': actStart.millisecondsSinceEpoch.toDouble(),
-            //   },
-            //   3: {
-            //     'id': "memId-$homeWidgetId",
-            //     'data': insertedMemId,
-            //   },
-            // };
-            // widgetTester.binding.defaultBinaryMessenger
-            //     .setMockMethodCallHandler(
-            //   const MethodChannel('home_widget'),
-            //   (message) {
-            //     if (message.method == 'registerBackgroundCallback') {
-            //       return Future.value(true);
-            //     } else if (message.method == 'saveWidgetData') {
-            //       expect(
-            //         message.arguments,
-            //         saveWidgetDataArgs[saveWidgetDataCount],
-            //       );
+                initializeCount++;
+                return Future.value(homeWidgetId);
+              },
+            );
+            final saveWidgetDataArgs = {
+              0: {
+                'id': "memName-$insertedMemId",
+                'data': insertedMemName,
+              },
+              1: {
+                'id': "actCount-$insertedMemId",
+                // length of inserted acts
+                'data': 1,
+              },
+              2: {
+                'id': "lastUpdatedAtSeconds-$insertedMemId",
+                'data': actStart.millisecondsSinceEpoch.toDouble(),
+              },
+              3: {
+                'id': "memId-$homeWidgetId",
+                'data': insertedMemId,
+              },
+            };
+            widgetTester.binding.defaultBinaryMessenger
+                .setMockMethodCallHandler(
+              const MethodChannel('home_widget'),
+              (message) {
+                if (message.method == 'registerBackgroundCallback') {
+                  return Future.value(true);
+                } else if (message.method == 'saveWidgetData') {
+                  expect(
+                    message.arguments,
+                    saveWidgetDataArgs[saveWidgetDataCount],
+                  );
 
-            //       saveWidgetDataCount++;
-            //       return Future.value(true);
-            //     } else if (message.method == 'updateWidget') {
-            //       expect(
-            //         message.arguments,
-            //         {
-            //           'name': "ActCounterProvider",
-            //           'android': null,
-            //           'ios': null,
-            //           'qualifiedAndroidName': null,
-            //         },
-            //       );
+                  saveWidgetDataCount++;
+                  return Future.value(true);
+                } else if (message.method == 'updateWidget') {
+                  expect(
+                    message.arguments,
+                    {
+                      'name': "ActCounterProvider",
+                      'android': null,
+                      'ios': null,
+                      'qualifiedAndroidName': null,
+                    },
+                  );
 
-            //       updateWidgetCount++;
-            //       return Future.value(true);
-            //     }
+                  updateWidgetCount++;
+                  return Future.value(true);
+                }
 
-            //     throw UnimplementedError();
-            //   },
-            // );
+                throw UnimplementedError();
+              },
+            );
 
             await launchActCounterConfigure();
             await widgetTester.pumpAndSettle();
@@ -189,15 +190,15 @@ void main() => group(
             await widgetTester.tap(find.byIcon(Icons.check).first);
             await widgetTester.pumpAndSettle();
 
-            // if (defaultTargetPlatform == TargetPlatform.android) {
-            //   // await expectLater(initializeCount, 1);
-            //   // await expectLater(saveWidgetDataCount, 4);
-            //   // await expectLater(updateWidgetCount, 1);
-            // } else {
-            //   await expectLater(initializeCount, 0);
-            //   await expectLater(saveWidgetDataCount, 0);
-            //   await expectLater(updateWidgetCount, 0);
-            // }
+            if (defaultTargetPlatform == TargetPlatform.android) {
+              await expectLater(initializeCount, 1);
+              await expectLater(saveWidgetDataCount, 4);
+              await expectLater(updateWidgetCount, 1);
+            } else {
+              await expectLater(initializeCount, 0);
+              await expectLater(saveWidgetDataCount, 0);
+              await expectLater(updateWidgetCount, 0);
+            }
           },
         );
 
