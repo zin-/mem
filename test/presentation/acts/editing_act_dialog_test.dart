ec946c95384d997e7408dc99c78d3770246a2210
zin- <noyu718@gmail.com>
2025-10-08 08:50:30 +0900

    !fixup

--- test/presentation/acts/editing_act_dialog_test.dart
+++ test/presentation/acts/editing_act_dialog_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mem/features/acts/list/item/editing_act_dialog.dart';
 
-const _name = 'EditingActDialog test';
-
 void main() {
-  group(_name, () {
+  group('EditingActDialog test', () {
     test('should create EditingActDialog widget', () {
       const dialog = EditingActDialog(1);
       expect(dialog, isA<EditingActDialog>());
