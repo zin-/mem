4ba70c8d9f2703dc5313d2c183aa3269f7ce36e1
zin- <noyu718@gmail.com>
2025-10-16 08:50:02 +0900

    Refactor EditingActDialog test: Simplify the test setup by introducing a fake act entities class and updating the dialog display logic to ensure proper behavior when the target act is not found.

--- test/presentation/acts/editing_act_dialog_test.dart
+++ test/presentation/acts/editing_act_dialog_test.dart
@@ -5,41 +5,23 @@ import 'package:mem/features/acts/list/item/editing_act_dialog.dart';
 import 'package:mem/features/acts/states.dart';
 import 'package:mem/features/acts/act_entity.dart';
 
+class _FakeActEntities extends ActEntities {
+  final Iterable<SavedActEntity> _state;
+
+  _FakeActEntities(this._state);
+
+  @override
+  Iterable<SavedActEntity> build() => _state;
+}
+
 void main() {
   group('EditingActDialog test', () {
-    testWidgets('should show dialog when actEntity is null', (tester) async {
-      await tester.pumpWidget(
-        ProviderScope(
-          child: MaterialApp(
-            home: Scaffold(
-              body: Builder(
-                builder: (context) => TextButton(
-                  onPressed: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => const EditingActDialog(1),
-                    );
-                  },
-                  child: const Text('Show Dialog'),
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-
-      await tester.tap(find.text('Show Dialog'));
-      await tester.pumpAndSettle();
-
-      expect(find.text('Show Dialog'), findsOneWidget);
-    });
-
-    testWidgets('should close dialog when editingActEntity is null',
+    testWidgets('should not show dialog when target act is not found.',
         (tester) async {
       await tester.pumpWidget(
         ProviderScope(
           overrides: [
-            actEntitiesProvider.overrideWith(() => _FakeActEntities()),
+            actEntitiesProvider.overrideWith(() => _FakeActEntities([])),
           ],
           child: MaterialApp(
             home: Scaffold(
@@ -63,11 +45,7 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.byType(AlertDialog), findsNothing);
+      expect(find.text('Show Dialog'), findsOneWidget);
     });
   });
-}
-
-class _FakeActEntities extends ActEntities {
-  @override
-  Iterable<SavedActEntity> build() => [];
 }
