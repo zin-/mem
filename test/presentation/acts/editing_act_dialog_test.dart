57d49e72572920d1e662fb7039880b45c9867125
zin- <noyu718@gmail.com>
2025-10-16 08:53:33 +0900

    Add test for EditingActDialog: Verify dialog display and interaction with a valid act entity, ensuring proper rendering of dialog components.

--- test/presentation/acts/editing_act_dialog_test.dart
+++ test/presentation/acts/editing_act_dialog_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mem/databases/table_definitions/acts.dart';
+import 'package:mem/databases/table_definitions/base.dart';
 import 'package:mem/features/acts/list/item/editing_act_dialog.dart';
 import 'package:mem/features/acts/states.dart';
 import 'package:mem/features/acts/act_entity.dart';
+import 'package:mem/framework/date_and_time/date_and_time_period_view.dart';
 
 class _FakeActEntities extends ActEntities {
   final Iterable<SavedActEntity> _state;
@@ -16,6 +19,54 @@ class _FakeActEntities extends ActEntities {
 
 void main() {
   group('EditingActDialog test', () {
+    testWidgets('should show dialog.', (tester) async {
+      final targetActEntity = SavedActEntity({
+        defPkId.name: 1,
+        defFkActsMemId.name: 1,
+        defColActsStart.name: DateTime.now(),
+        defColActsStartIsAllDay.name: false,
+        defColActsEnd.name: null,
+        defColActsEndIsAllDay.name: null,
+        defColActsPausedAt.name: null,
+        defColCreatedAt.name: DateTime.now(),
+        defColUpdatedAt.name: DateTime.now(),
+        defColArchivedAt.name: null,
+      });
+
+      await tester.pumpWidget(
+        ProviderScope(
+          overrides: [
+            actEntitiesProvider
+                .overrideWith(() => _FakeActEntities([targetActEntity])),
+          ],
+          child: MaterialApp(
+            home: Scaffold(
+              body: Builder(
+                builder: (context) => TextButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) =>
+                          EditingActDialog(targetActEntity.id),
+                    );
+                  },
+                  child: const Text('Show Dialog'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.text('Show Dialog'));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(AlertDialog), findsOneWidget);
+      expect(find.byType(DateAndTimePeriodTextFormFields), findsOneWidget);
+      expect(find.byIcon(Icons.delete), findsOneWidget);
+      expect(find.byIcon(Icons.save_alt), findsOneWidget);
+    });
+
     testWidgets('should not show dialog when target act is not found.',
         (tester) async {
       await tester.pumpWidget(
