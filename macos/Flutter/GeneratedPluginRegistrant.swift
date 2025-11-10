376c5ce2c9f2cf050802167d5a08ba626ca008f0
zin- <noyu718@gmail.com>
2025-11-10 09:42:04 +0900

    Update dependencies and add new plugins
    - Added new dependencies: `drift`, `drift_flutter`, `sqlite3_flutter_libs`, and `sqlparser`.
    - Updated existing dependencies to their latest versions, including `build`, `custom_lint`, `flutter_riverpod`, and others.
    - Modified `pubspec.yaml` to reflect the new structure and removed commented-out sections for clarity.
    - Registered new plugins in platform-specific files for Linux, macOS, and Windows.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -13,6 +13,7 @@ import sentry_flutter
 import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
+import sqlite3_flutter_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
@@ -23,4 +24,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
 }
