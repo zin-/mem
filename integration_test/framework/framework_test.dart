15416df271a26fae2bd7c46c7f2f8bacd3a44585
zin- <noyu718@gmail.com>
2026-01-19 13:14:52 +0900

    Refactor integration tests to improve structure and add native to Drift migration test
    - Updated import paths in `framework_test.dart` for consistency.
    - Introduced a new file `native_to_drift_migration_test.dart` containing comprehensive tests for validating data migration from the native database to the Drift database.
    - The new test includes setup and teardown procedures to ensure a clean environment and verifies the integrity of various data types post-migration.

--- integration_test/framework/framework_test.dart
+++ integration_test/framework/framework_test.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mem/features/logger/log.dart';
 import 'package:mem/features/logger/log_service.dart';
-import '../databases/native_to_drift_migration_test.dart'
-    as native_to_drift_migration_test;
+import 'native_to_drift_migration_test.dart' as native_to_drift_migration_test;
 import 'database_accessor_tests.dart' as database_accessor_tests;
 import 'database_factory_tests.dart' as database_factory_tests;
 import 'database_repository_tests.dart' as database_repository_tests;
