24ef1d95f1d1a4dd4c25740bad0d093e1d9918a7
zin- <noyu718@gmail.com>
2026-01-21 08:15:39 +0900

    Add setup and teardown procedures for migration tests in native_to_drift.dart
    - Introduced setup functions to initialize the database and clear existing tables before each test, ensuring a clean state for accurate testing.
    - Added logging of native mems after migration to facilitate debugging and validation of the migration process.
    - Enhanced the overall structure of migration tests for better organization and clarity.

--- integration_test/framework/migrations/native_to_drift.dart
+++ integration_test/framework/migrations/native_to_drift.dart
@@ -3,6 +3,7 @@ import 'package:mem/databases/definition.dart';
 import 'package:mem/databases/migrations/native_to_drift.dart';
 import 'package:mem/databases/table_definitions/base.dart';
 import 'package:mem/databases/table_definitions/mems.dart';
+import 'package:mem/features/logger/log_service.dart';
 import 'package:mem/framework/database/accessor.dart';
 import 'package:mem/framework/database/factory.dart';
 
@@ -10,6 +11,17 @@ const _name = "Native to Drift tests";
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    DatabaseFactory.onTest = true;
+  });
+  setUp(() async {
+    final nativeDatabaseAccessor =
+        await DatabaseFactory.open(databaseDefinition);
+    for (var tableDefinition in databaseDefinition.tableDefinitions) {
+      await nativeDatabaseAccessor.delete(tableDefinition);
+    }
+  });
 
   group(_name, () {
     group("Migrations", () {
@@ -24,6 +36,9 @@ void main() {
 
         final driftDatabaseAccessor = DriftDatabaseAccessor();
         await migrateNativeToDrift(driftDatabaseAccessor.driftDatabase);
+
+        final nativeMems = await nativeDatabaseAccessor.select(defTableMems);
+        debug("nativeMems: $nativeMems");
       });
     });
   });
